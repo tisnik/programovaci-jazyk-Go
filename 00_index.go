665f14d96dc42eef78fbef4e44c809b3393f850e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-01 10:41:16 +0100

    Index file

---
+++ 00_index.go
@@ -0,0 +1,33 @@
+// Demonstrační příklady použité v knize:
+// Programovací jazyk Go
+//
+// Obsah podadresářů:
+//
+// 05_zaciname:
+//
+// 06_zaklady:
+//
+// 07_standardni_datove_typy:
+//
+// 08_slozene_datove_typy:
+//
+// 09_ukazatele:
+//
+// 10_soubeznost_a_paralelnost:
+//
+// 11_rozhrani:
+//
+// 12_uzivatelske_datove_typy:
+//
+// 13_genericke_datove_typy:
+//
+// 14_zakladni_knihovna:
+//
+// 15_testovani:
+//
+// 16_ladeni:
+//
+// 17_web_assembly:
+//
+// 18_gopherjs:
+// 
