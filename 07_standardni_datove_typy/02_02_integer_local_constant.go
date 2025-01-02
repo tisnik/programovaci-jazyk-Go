7e84bc8358c95a8eeb7d8b1a4032191e6d1ebf1f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-02 09:08:27 +0100

    Local integer constant

---
+++ 07_standardni_datove_typy/02_02_integer_local_constant.go
@@ -0,0 +1,15 @@
+// Základní datové typy v jazyce Go
+//
+// - celočíselná hodnota deklarovaná jako lokální konstanta
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace lokální celočíselné konstanty
+	const Answer = 42
+
+	// tisk hodnoty lokální konstanty
+	fmt.Println(Answer)
+}
