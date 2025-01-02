2862aace0198a9cd6cd12c98d71ef1ae75204cd7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-02 09:08:27 +0100

    Global integer constant

---
+++ 07_standardni_datove_typy/02_01_integer_global_constant.go
@@ -0,0 +1,15 @@
+// Základní datové typy v jazyce Go
+//
+// - celočíselná hodnota deklarovaná jako globální konstanta
+
+package main
+
+import "fmt"
+
+// deklarace globální celočíselné konstanty
+const Answer = 42
+
+func main() {
+	// tisk hodnoty globální konstanty
+	fmt.Println(Answer)
+}
