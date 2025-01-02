2ee118ac40d46fb39ad4502268a5bc2a5b7da95b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-02 09:08:27 +0100

    Identifiers for standard data types

---
+++ 07_standardni_datove_typy/01_01_standard_types_identifiers.go
@@ -0,0 +1,35 @@
+// Základní datové typy v jazyce Go
+//
+// - deklarace proměnných, z nichž každá je odlišného
+//   základního datového typu (nebo jeho aliasu)
+
+package main
+
+// globální proměnné automaticky inicializované na nulové hodnoty
+var (
+	a       any
+	b       bool
+	flags   byte
+	c1      complex64
+	c2      complex128
+	e       error
+	f1      float32
+	f2      float64
+	i1      int
+	i2      int8
+	i3      int16
+	i4      int32
+	i5      int64
+	znak    rune
+	zprava  string
+	u1      uint
+	u2      uint8
+	u3      uint16
+	u4      uint32
+	u5      uint64
+	pointer uintptr
+)
+
+func main() {
+
+}
