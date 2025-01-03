aff7f67f4244d6af441523353256ef7f48449212
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-03 09:48:55 +0100

    Difference between int and uint

---
+++ 07_standardni_datove_typy/02_11_int_uint.go
@@ -0,0 +1,16 @@
+// Datové typy int a uint.
+
+package main
+
+import "fmt"
+
+func main() {
+	// lokální proměnná typu int
+	var x int = -10
+
+	// lokální proměnná typu uint
+	var y uint = 10
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
