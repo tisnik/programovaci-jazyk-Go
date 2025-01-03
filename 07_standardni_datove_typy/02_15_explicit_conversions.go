fef9a6b91ae693adbb23ff6075a0a0d1fed866fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-03 09:48:55 +0100

    Explicit type conversions

---
+++ 07_standardni_datove_typy/02_15_explicit_conversions.go
@@ -0,0 +1,37 @@
+// Explicitní konverze mezi hodnotami různých datových typů.
+
+package main
+
+import "fmt"
+
+func main() {
+	// příprava proměnných, jejichž hodnoty se budou později
+	// konvertovat na jiné datové typy
+	var a int8 = -10
+	var signedInt int32 = -100000
+	var unsignedInt uint32 = 100000
+	var e float32 = 1e4
+	var f float64 = 1.5e30
+
+	// konverze hodnot
+	var x int32 = int32(a)
+
+	// zde dochází k zaokrouhlení
+	var y int32 = int32(e)
+
+	// zde dochází ke ztrátě přesnosti
+	// a může dojít i k přetečení výsledku
+	var z float32 = float32(f)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	// konverze hodnot, dochází zde ke zmenšení
+	// rozahu a k přetečení výsledků
+	var b2 uint8 = uint8(signedInt)
+	var b3 uint8 = uint8(unsignedInt)
+
+	fmt.Println(b2)
+	fmt.Println(b3)
+}
