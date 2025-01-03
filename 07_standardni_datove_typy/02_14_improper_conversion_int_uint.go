006318167e3413a173b3d31f928488b79f543a9c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-03 09:48:55 +0100

    Improper type conversions between int and uint

---
+++ 07_standardni_datove_typy/02_14_improper_conversion_int_uint.go
@@ -0,0 +1,23 @@
+// Striktní kontroly prováděné překladačem při konverzi mezi datovými typy.
+
+package main
+
+import "fmt"
+
+func main() {
+	var a int8 = 100
+
+	// pokus o implicitní konverzi mezi typem int8 a uint8
+	var b uint8 = a
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+	var c uint8 = 100
+
+	// pokus o implicitní konverzi mezi typem uint8 a int8
+	var d int8 = c
+
+	fmt.Println(c)
+	fmt.Println(d)
+}
