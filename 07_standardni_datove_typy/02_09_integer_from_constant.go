8d8b63e3dcee204c546c26f28709afcf782af60e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-03 09:48:55 +0100

    Variables initialized from integer constants

---
+++ 07_standardni_datove_typy/02_09_integer_from_constant.go
@@ -0,0 +1,24 @@
+//
+
+package main
+
+import "fmt"
+
+// globální celočíselná konstanta
+const Answer = 42
+
+func main() {
+	// typ odpovídající osmibitovému, 16bitovému, 32bitovému
+	// a 64bitovému číslu bez znaménka
+	var a uint8 = Answer
+	var b uint16 = Answer
+	var c uint32 = Answer
+	var d uint64 = Answer
+
+	// zjištění, jaký výchozí formát je použit pro tisk celočíselných
+	// hodnot se znaménkem
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
