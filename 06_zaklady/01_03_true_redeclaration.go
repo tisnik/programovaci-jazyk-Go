6ead115a18fb35fdfb05d6591f556536fe75c8f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:24:59 +0100

    true value redeclaration

---
+++ 06_zaklady/01_03_true_redeclaration.go
@@ -0,0 +1,20 @@
+// Základy programovacího jazyka Go
+//
+// - deklarace proměnné "true", která přepíše původní hodnotu "true"
+//
+// POZOR: tento způsob není doporučený a je dobré vědět, kdy ho použít
+//        a především, kdy ho naopak nepoužít.
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace proměnné nazvané "true", tím v tomto balíčku ztratíme
+	// přístup k původní hodnotě true (což většinou nechceme!)
+	var true string = "pravda"
+
+	// deklarace proměnné, která je ve skutečnosti typu "bool"
+	var answer string = true
+	fmt.Println(answer)
+}
