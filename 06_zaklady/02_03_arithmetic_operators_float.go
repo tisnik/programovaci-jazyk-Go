929fc41655e8e1896a2c46e9700ca1eda73f7854
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:24:59 +0100

    Arithmetic operators and float variables

---
+++ 06_zaklady/02_03_arithmetic_operators_float.go
@@ -0,0 +1,26 @@
+// Základy programovacího jazyka Go
+//
+// - základní aritmetické operátory s proměnnými
+//   typu float32
+
+package main
+
+import "fmt"
+
+func main() {
+	// dvojice proměnných typu float32
+	var x float32 = 1.1
+	var y float32 = 2.2
+
+	// výpis původních hodnot
+	fmt.Printf("x = %f\n", x)
+	fmt.Printf("y = %f\n", y)
+
+	fmt.Println()
+
+	// výpis výsledků operací
+	fmt.Printf("x+y = %f\n", x+y)
+	fmt.Printf("x-y = %f\n", x-y)
+	fmt.Printf("x*y = %f\n", x*y)
+	fmt.Printf("x/y = %f\n", x/y)
+}
