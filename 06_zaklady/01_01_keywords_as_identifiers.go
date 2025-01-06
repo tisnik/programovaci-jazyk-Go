6673d4d8d097c30997437cf4136769035eaa75d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:24:59 +0100

    Keywords and identifiers

---
+++ 06_zaklady/01_01_keywords_as_identifiers.go
@@ -0,0 +1,17 @@
+// Základy programovacího jazyka Go
+//
+// - pokus o použití klíčových slov ve funkci identifikátorů
+//
+// POZOR: tento příklad nebude možné přeložit
+
+package main
+
+func main() {
+	// pokus o použití klíčového slova "interface"
+	// jako identifikátoru
+	const interface int = 10
+
+	// pokus o použití klíčového slova "type"
+	// jako identifikátoru
+	var type string = "Škoda 100"
+}
