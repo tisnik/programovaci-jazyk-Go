27ec54d6afd3cfcb562243b34192cdfa037fc4b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-07 10:03:14 +0100

    Logical operators usage

---
+++ 06_zaklady/02_11_logical_operators.go
@@ -0,0 +1,25 @@
+// Základy programovacího jazyka Go
+//
+// - logické operátory
+
+package main
+
+import "fmt"
+
+func main() {
+	// dvojice proměnných typu bool(ean)
+	var x bool = true
+	var y bool = false
+
+	// výpis původních hodnot
+	fmt.Printf("x = %t\n", x)
+	fmt.Printf("y = %t\n", y)
+
+	fmt.Println()
+
+	// výpis výsledků operací
+	fmt.Printf("not x = %t\n", !x)
+	fmt.Printf("not y = %t\n", !y)
+	fmt.Printf("x && y = %t\n", x && y)
+	fmt.Printf("x || y = %t\n", x || y)
+}
