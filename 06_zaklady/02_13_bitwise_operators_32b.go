af17fdf279c62b7f2f0f731cf0451dc65d2db37a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-07 10:03:14 +0100

    Bitwise operators and 32bit operands

---
+++ 06_zaklady/02_13_bitwise_operators_32b.go
@@ -0,0 +1,26 @@
+// Základy programovacího jazyka Go
+//
+// - bitové operátory pro 32bitové hodnoty
+
+package main
+
+import "fmt"
+
+func main() {
+	// dvojice proměnných typu uint32
+	// (32bitové celé číslo bez znaménka)
+	var x uint32 = 42
+	var y uint32 = 100
+
+	// výpis původních hodnot
+	fmt.Printf("x =      %032b\n", x)
+	fmt.Printf("y =      %032b\n", y)
+
+	// výpis výsledků operací
+	fmt.Printf("neg x =  %032b\n", ^x)
+	fmt.Printf("neg y =  %032b\n", ^y)
+	fmt.Printf("x && y = %032b\n", x&y)
+	fmt.Printf("x || y = %032b\n", x|y)
+	fmt.Printf("x ^ y =  %032b\n", x^y)
+	fmt.Printf("x &^ y = %032b\n", x&^y)
+}
