d3f5e506fd5836fe6f2cef2628507c8aa5deed03
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-07 10:03:14 +0100

    Bit shifting int value

---
+++ 06_zaklady/02_15_bit_shifts_int.go
@@ -0,0 +1,20 @@
+// Základy programovacího jazyka Go
+//
+// - bitové posuny pro hodnoty typu int
+
+package main
+
+import "fmt"
+
+func main() {
+	// celočíselný typ se znaménkem
+	var x int = -42
+
+	// zobrazení původní hodnoty x
+	fmt.Printf("x =     %08b\n", x)
+
+	// zobrazení hodnoty x po jejím posunu doprava a doleva
+	// o jeden bit
+	fmt.Printf("x>>1 =  %08b\n", x>>1)
+	fmt.Printf("x<<1 =  %08b\n", x<<1)
+}
