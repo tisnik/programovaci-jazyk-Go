e25253abfc0b1f7c12f659780eaca1567cc4102c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-07 10:03:14 +0100

    Bitwise operators and 8bit operands

---
+++ 06_zaklady/02_12_bitwise_operators_8b.go
@@ -0,0 +1,26 @@
+// Základy programovacího jazyka Go
+//
+// - bitové operátory pro osmibitové hodnoty
+
+package main
+
+import "fmt"
+
+func main() {
+	// dvojice proměnných typu byte
+	// (osmibitové celé číslo bez znaménka)
+	var x byte = 42
+	var y byte = 100
+
+	// výpis původních hodnot
+	fmt.Printf("x =      %08b\n", x)
+	fmt.Printf("y =      %08b\n", y)
+
+	// výpis výsledků operací
+	fmt.Printf("neg x =  %08b\n", ^x)
+	fmt.Printf("neg y =  %08b\n", ^y)
+	fmt.Printf("x && y = %08b\n", x&y)
+	fmt.Printf("x || y = %08b\n", x|y)
+	fmt.Printf("x ^ y =  %08b\n", x^y)
+	fmt.Printf("x &^ y = %08b\n", x&^y)
+}
