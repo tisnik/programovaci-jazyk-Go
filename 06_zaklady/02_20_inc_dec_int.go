7ca93a143f50504a1ecd397cbae52c7ee90c1cd9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-08 09:34:20 +0100

    Increment and decrement operations on integers

---
+++ 06_zaklady/02_20_inc_dec_int.go
@@ -0,0 +1,27 @@
+// Základy programovacího jazyka Go
+//
+// - operátory ++ a -- použité pro proměnné typu int
+
+package main
+
+import "fmt"
+
+func main() {
+	// původní hodnota x
+	var x int = 1
+
+	// výpis původní hodnoty
+	fmt.Printf("x = %d\n", x)
+
+	// zvýšení hodnoty x o jedničku
+	x++
+
+	// výpis nové hodnoty
+	fmt.Printf("x = %d\n", x)
+
+	// snížení hodnoty x o jedničku
+	x--
+
+	// výpis nové hodnoty
+	fmt.Printf("x = %d\n", x)
+}
