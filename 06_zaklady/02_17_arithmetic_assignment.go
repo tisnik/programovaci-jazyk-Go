72212745e94beaf830734e359a294b69b53f594b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-08 09:34:20 +0100

    Arithmetic operators with assignment

---
+++ 06_zaklady/02_17_arithmetic_assignment.go
@@ -0,0 +1,32 @@
+// Základy programovacího jazyka Go
+//
+// - základní aritmetické operátory kombinované
+//   s přiřazením
+
+package main
+
+import "fmt"
+
+func main() {
+	// původní hodnota x
+	var x int = 1
+
+	// zobrazení původní hodnoty x
+	fmt.Printf("x = %d\n", x)
+
+	// modifikace x aritmetickou operací +
+	x += 10
+	fmt.Printf("x = %d\n", x)
+
+	// modifikace x aritmetickou operací *
+	x *= 2
+	fmt.Printf("x = %d\n", x)
+
+	// modifikace x aritmetickou operací /
+	x /= 3
+	fmt.Printf("x = %d\n", x)
+
+	// modifikace x aritmetickou operací %
+	x %= 2
+	fmt.Printf("x = %d\n", x)
+}
