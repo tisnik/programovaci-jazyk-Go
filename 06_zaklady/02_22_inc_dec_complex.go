90ec4e8039598c99a06cd02943dbd7595ee9caf4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-08 09:34:20 +0100

    Increment and decrement operations on complex numbers

---
+++ 06_zaklady/02_22_inc_dec_complex.go
@@ -0,0 +1,21 @@
+// Základy programovacího jazyka Go
+//
+// - operátory ++ a -- použité pro proměnné typu complex
+
+package main
+
+import "fmt"
+
+func main() {
+	// původní hodnota z
+	var z complex64 = 1 + 2i
+
+	// výpis původní hodnoty
+	fmt.Printf("z = %v\n", z)
+
+	// zvýšení hodnoty x o jedničku
+	z++
+
+	// výpis nové hodnoty
+	fmt.Printf("z = %v\n", z)
+}
