627c8f4a05469447331706e52a81c72a08dbd5ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-08 09:34:20 +0100

    Bit shift operators with assignment

---
+++ 06_zaklady/02_18_bit_shift_assignment.go
@@ -0,0 +1,34 @@
+// Základy programovacího jazyka Go
+//
+// - operátory pro bitové posuny kombinované
+//   s přiřazením
+
+package main
+
+import "fmt"
+
+func main() {
+	// původní hodnota x
+	var x int = 1
+
+	// postupné násobení hodnoty x dvojkou
+	for i := uint(0); i <= 10; i++ {
+		fmt.Printf("%d << %2d == %4d\n", 1, i, x)
+		// výpočet další hodnoty v řadě
+		x <<= 1
+	}
+
+	fmt.Println()
+
+	// původní hodnota x
+	x = 10000000
+
+	// postupné dělení hodnoty x dvojkou
+	// (i musí být numerický typ bez znaménka)
+	for i := uint(0); i <= 10; i++ {
+		fmt.Printf("%d >> %2d == %4d\n", 1, i, x)
+		// výpočet další hodnoty v řadě
+		x >>= 1
+	}
+
+}
