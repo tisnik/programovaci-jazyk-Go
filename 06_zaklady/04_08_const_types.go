855cd19bd2434b16e9105d23b61ef71eb995b613
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-09 09:07:11 +0100

    Constants and types, 4th example

---
+++ 06_zaklady/04_08_const_types.go
@@ -0,0 +1,31 @@
+// Základy programovacího jazyka Go
+//
+// - konstanta bez explicitně definovaného typu
+// - konstanta s definovaným typem hodnoty
+// - výpis typů hodnot konstant
+// - výpis typů proměnných, do kterých je přiřazena konstanta
+
+package main
+
+import "fmt"
+
+const (
+	zprava1        = "foo"
+	zprava2 string = "bar"
+)
+
+func main() {
+	// v deklaraci těchto proměnných není explicitně zapsán typ
+	// (ten je odvozen překladačem automaticky)
+	var v1 = zprava1
+	var v2 = zprava2
+
+	// vypsat hodnoty proměnných
+	fmt.Println(v1, v2)
+
+	// vypsat typy konstant
+	fmt.Printf("%T %T\n", zprava1, zprava2)
+
+	// vypsat typy proměnných
+	fmt.Printf("%T %T\n", v1, v2)
+}
