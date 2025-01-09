1d0512363fdd400a22a17b0166ad54e0a8a14a5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-09 09:07:11 +0100

    Constants and types, 2nd example

---
+++ 06_zaklady/04_06_const_types.go
@@ -0,0 +1,29 @@
+// Základy programovacího jazyka Go
+//
+// - konstanta bez explicitně definovaného typu
+// - konstanta s definovaným typem hodnoty
+// - přiřazení pojmenovaných konstant do proměnných
+// - použití pojmenovaných konstant ve výrazu
+
+package main
+
+import "fmt"
+
+const (
+	zprava1        = "foo"
+	zprava2 string = "bar"
+)
+
+func main() {
+	var v1, v2 string
+	// přiřazení pojmenovaných konstant do proměnných
+	v1 = zprava1
+	v2 = zprava2
+	fmt.Println(v1, v2)
+
+	var v3 string
+
+	// pojmenované konstanty použité ve výrazu
+	v3 = zprava1 + zprava2 + "."
+	fmt.Println(v3)
+}
