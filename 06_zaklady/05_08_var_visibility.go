a046ac00451db00b13177cf0ed45ce3a02b90963
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-10 09:25:47 +0100

    Variables scope + visibility, example #1

---
+++ 06_zaklady/05_08_var_visibility.go
@@ -0,0 +1,41 @@
+// Základy programovacího jazyka Go
+//
+// - oblast viditelnosti proměnných
+
+package main
+
+import "fmt"
+
+// globální proměnná
+var x = 1
+
+func main() {
+	fmt.Println(x)
+
+	// lokální proměnná pro funkci
+	var x = 2
+	fmt.Println(x)
+
+	if true {
+		// lokální proměnná pro blok
+		var x = 3
+		fmt.Println(x)
+
+		// modifikace lokální proměnné
+		x += 100
+		fmt.Println(x)
+	}
+	// výpis lokální proměnné (v bloku se nezměnila)
+	fmt.Println(x)
+
+	// modifikace lokální proměnné (nikoli proměnné globální)
+	x += 100
+	fmt.Println(x)
+
+	// tisk původní hodnoty globální proměnné
+	printGlobalX()
+}
+
+func printGlobalX() {
+	fmt.Println(x)
+}
