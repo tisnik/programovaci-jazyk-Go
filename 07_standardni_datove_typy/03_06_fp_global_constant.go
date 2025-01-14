afe2f019a964823621d264ad5919a9fed870346b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-14 08:50:24 +0100

    Global floating point constants

---
+++ 07_standardni_datove_typy/03_06_fp_global_constant.go
@@ -0,0 +1,15 @@
+// Základní datové typy v jazyce Go
+//
+// - hodnota s plovoucí čárkou deklarovaná jako globální konstanta
+
+package main
+
+import "fmt"
+
+// deklarace globální konstanty
+const Pi = 3.1415
+
+func main() {
+	// tisk hodnoty konstanty
+	fmt.Println(Pi)
+}
