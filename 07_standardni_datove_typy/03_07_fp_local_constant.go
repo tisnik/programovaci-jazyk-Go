04f5264157b3d5795221b3f5aa1269f67d75cc85
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-15 09:36:24 +0100

    Local FP constant

---
+++ 07_standardni_datove_typy/03_07_fp_local_constant.go
@@ -0,0 +1,15 @@
+// Základní datové typy v jazyce Go
+//
+// - hodnota s plovoucí čárkou deklarovaná jako lokální konstanta
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace lokální konstanty
+	const Pi = 3.1415
+
+	// tisk hodnoty konstanty
+	fmt.Println(Pi)
+}
