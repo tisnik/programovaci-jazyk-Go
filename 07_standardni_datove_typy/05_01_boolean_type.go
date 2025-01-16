b17afc103670667e587feb92f853d6b4f4fdd988
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-16 09:12:53 +0100

    Boolean data type

---
+++ 07_standardni_datove_typy/05_01_boolean_type.go
@@ -0,0 +1,14 @@
+// Datový typ bool a pravdivostní hodnoty v programovacím jazyku Go.
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace a inicializace proměnných typu `bool`
+	var a bool = true
+	var b bool = false
+
+	fmt.Println(a)
+	fmt.Println(b)
+}
