7bb884582ea49bfa5f1aa35c44cbaa2beb7d4faa
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-17 09:01:12 +0100

    Multiple return values that can be ignored

---
+++ 06_zaklady/07_11_multiple_return_ignore.go
@@ -0,0 +1,24 @@
+// Základy programovacího jazyka Go
+//
+// - deklarace funkce s parametry a s návratovými hodnotami
+// - parametry stejného typu jsou deklarovány společně
+// - ignorování jedné návratové hodnoty
+
+package main
+
+import "fmt"
+
+// Deklarace funkce s parametry a s návratovými hodnotami
+func calcSumProd(x, y int) (int, int) {
+	sum := x + y
+	prod := x * y
+
+	// návrat s předáním dvou návratových hodnot
+	return sum, prod
+}
+
+func main() {
+	// zavolání funkce s předáním parametrů
+	s, _ := calcSumProd(2, 3)
+	fmt.Printf("sum  = %d\n", s)
+}
