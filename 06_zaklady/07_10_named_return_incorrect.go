d4aa1d2b2ab6f894c4d513f9486c3cc3e5208d5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-17 09:01:12 +0100

    Incorrect usage of named return statement

---
+++ 06_zaklady/07_10_named_return_incorrect.go
@@ -0,0 +1,26 @@
+// Základy programovacího jazyka Go
+//
+// - deklarace funkce s parametry a s návratovými hodnotami
+// - pojmenování návratových hodnot
+// - parametry stejného typu jsou deklarovány společně
+// - nekorektní deklarace proměnných, které se jmenují stejně, jako parametry
+
+package main
+
+import "fmt"
+
+// Deklarace funkce s parametry a s návratovými hodnotami
+func calcSumProd(x, y int) (sum, prod int) {
+	sum := x + y
+	prod := x * y
+
+	// návrat
+	return
+}
+
+func main() {
+	// zavolání funkce s předáním parametrů
+	s, p := calcSumProd(2, 3)
+	fmt.Printf("sum  = %d\n", s)
+	fmt.Printf("prod = %d\n", p)
+}
