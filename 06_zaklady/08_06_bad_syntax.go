f7e21b6c07b165a41e503de95c37978dbf62fc79
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 09:05:53 +0100

    Improper syntax of if-else construct

---
+++ 06_zaklady/08_06_bad_syntax.go
@@ -0,0 +1,26 @@
+// Základy programovacího jazyka Go
+//
+// - příklady špatné syntaxe konstrukce if-else.
+// POZOR: tento příklad nebude možné přeložit
+
+package main
+
+func classify_char(c rune) string {
+	if c >= 'a' && c <= 'z'
+	{
+		return "male pismeno"
+	}
+	else if c >= 'A' && c <= 'Z' {
+		return "velke pismeno"
+	}
+	else
+	{
+		return "neco jineho"
+	}
+}
+
+func main() {
+	println(classify_char('a'))
+	println(classify_char('Z'))
+	println(classify_char('?'))
+}
