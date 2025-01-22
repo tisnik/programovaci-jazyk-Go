462369f2b238414ae6cb2b2d05919c6de1b95583
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-22 08:44:40 +0100

    Index file

---
+++ 18_gopherjs/00_index.go
@@ -0,0 +1,35 @@
+// Transpiler GopherJS
+//
+// Seznam příkladů uložených v tomto podadresáři:
+// 00_index.go
+// dom_add_element.go
+// dom_add_element.html
+// dom_manipulation.go
+// dom_manipulation.html
+// draw_into_canvas.go
+// draw_into_canvas.html
+// func_call_argument_types_1.go
+// func_call_argument_types_1.html
+// func_call_argument_types_2.go
+// func_call_argument_types_2.html
+// func_call_no_arguments.go
+// func_call_no_arguments.html
+// func_call_return_int_1.go
+// func_call_return_int_1.html
+// func_call_return_int_2.go
+// func_call_return_int_2.html
+// func_call_two_floats.go
+// func_call_two_floats.html
+// func_call_two_ints.go
+// func_call_two_ints.html
+// func_call_with_arguments.go
+// func_call_with_arguments.html
+// hello_world_2.go
+// hello_world_2.html
+// hello_world.go
+// hello_world.html
+// http_server.go
+// js_interop_1.go
+// js_interop_1.html
+// js_interop_2.go
+// js_interop_2.html
