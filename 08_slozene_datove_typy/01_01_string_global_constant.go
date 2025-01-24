4de46cabe330f52ab2765ccf4c96dfda1b5f29de
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-24 08:35:35 +0100

    Global string constant

---
+++ 08_slozene_datove_typy/01_01_string_global_constant.go
@@ -0,0 +1,15 @@
+// Řetězce v programovacím jazyce Go
+//
+// - deklarace globální konstanty s řetězcem
+
+package main
+
+import "fmt"
+
+// deklarace konstanty typu řetězec
+const message = "Toto je řetězec!"
+
+func main() {
+	// tisk konstanty
+	fmt.Println(message)
+}
