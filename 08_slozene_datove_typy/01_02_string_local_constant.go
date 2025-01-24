d51a15664dbf6c31294c964c974926188b09d634
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-24 08:35:35 +0100

    Local string constant

---
+++ 08_slozene_datove_typy/01_02_string_local_constant.go
@@ -0,0 +1,15 @@
+// Řetězce v programovacím jazyce Go
+//
+// - deklarace lokální konstanty s řetězcem
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace konstanty typu řetězec
+	const message = "Toto je řetězec!"
+
+	// tisk konstanty
+	fmt.Println(message)
+}
