8c252390c0b1a72b42d1bf931e582e6ee4b6de08
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-24 08:35:35 +0100

    Printing strings

---
+++ 08_slozene_datove_typy/01_07_strings.go
@@ -0,0 +1,32 @@
+// Řetězce v programovacím jazyce Go
+//
+// - prázdný řetězec
+// - řetězce s řídicími znaky
+// - řetězce s Unicode znaky
+
+package main
+
+import "fmt"
+
+func main() {
+	// prázdný řetězec
+	var s1 string
+
+	// prázdný řetězec
+	var s2 string = ""
+
+	// řetězec s řídicími znaky
+	var s3 string = "Hello\nworld!"
+
+	// řetězec s Unicode znaky
+	var s4 string = "ěščřžýáíéů"
+
+	// řetězec s Unicode znaky
+	var s5 string = "шщэюя"
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+	fmt.Println(s5)
+}
