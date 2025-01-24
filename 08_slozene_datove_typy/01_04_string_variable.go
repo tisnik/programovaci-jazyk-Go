0f8ca6fa00da30b092331f59ec8b22e9638cfb13
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-24 08:35:35 +0100

    String variable

---
+++ 08_slozene_datove_typy/01_04_string_variable.go
@@ -0,0 +1,16 @@
+// Řetězce v programovacím jazyce Go
+//
+// - deklarace proměnné typu řetězec
+// - tisk obsahu této proměnné
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace proměnné typu řetězec
+	var message string = "Toto je retezec!"
+
+	// tisk obsahu této proměnné
+	fmt.Println(message)
+}
