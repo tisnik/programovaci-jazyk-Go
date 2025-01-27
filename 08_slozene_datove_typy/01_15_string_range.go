c798e1c30e652d2cd78c09a267a00c691eb75aae
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-27 10:32:18 +0100

    Strings and range keyword

---
+++ 08_slozene_datove_typy/01_15_string_range.go
@@ -0,0 +1,17 @@
+// Řetězce v programovacím jazyce Go
+//
+// - řetězec s Unicode znaky
+// - průchod jednotlivými znaky řetězce
+// - tisk kódů znaků
+
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Příliš žluťoučký kůň"
+
+	for _, c := range s {
+		fmt.Printf("%02x ", c)
+	}
+}
