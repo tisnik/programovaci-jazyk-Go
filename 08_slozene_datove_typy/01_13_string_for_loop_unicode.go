4fe98de8163f970c63a955674c8d253b5bc36025
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-27 10:32:18 +0100

    String and loops on Unicode string

---
+++ 08_slozene_datove_typy/01_13_string_for_loop_unicode.go
@@ -0,0 +1,17 @@
+// Řetězce v programovacím jazyce Go
+//
+// - řetězec obsahující Unicode znaky
+// - průchod jednotlivými bajty tvořícími řetězec
+
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Příliš žluťoučký kůň"
+
+	// průchod přes jednotlivé bajty
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%c", s[i])
+	}
+}
