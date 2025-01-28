4789cd33ca8871008fbe6860628a09dde5e107e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-28 09:24:01 +0100

    Hello world written in Go

---
+++ 05_zaciname/hello_world.go
@@ -0,0 +1,17 @@
+// První kroky v jazyku Go
+//
+// - program typu "Hello, world!" ve variantě naprogramované
+//   v jazyku Go.
+
+// specifikace jména balíčku (zde musíme použít "main")
+package main
+
+// import konstant, typů a funkcí z jiného balíčku
+import "fmt"
+
+// deklarace funkce nazvané main
+func main() {
+	// volání funkce náležející do jiného balíčku
+	// s využitím takzvané tečkové notace
+	fmt.Println("Hello, world!")
+}
