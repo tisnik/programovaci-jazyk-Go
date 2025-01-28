52b8c677f59ce96d4c831f32504b7399df493fb4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-28 09:24:01 +0100

    Comments in sources, example #4

---
+++ 05_zaciname/comments_4.go
@@ -0,0 +1,13 @@
+package main
+
+/* Funkce main, která slouží jako vstupní bod do aplikace. */
+func main() {
+}
+
+// Zakomentovaný kus zdrojového kódu
+
+/*
+func main() {
+    /* vložený komentář */
+}
+*/
