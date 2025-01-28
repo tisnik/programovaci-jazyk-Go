e4c606103630149d1508b3cd4ddd8c2d0a38cbda
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-28 09:24:01 +0100

    Comments in sources, example #2

---
+++ 05_zaciname/comments_2.go
@@ -0,0 +1,11 @@
+/*
+	Jméno balíčku.
+
+Balíček main má specifický význam pro běžné programy, ovšem
+v knihovnách se nepoužívá.
+*/
+package main
+
+/* Funkce main, která slouží jako vstupní bod do aplikace. */
+func main() {
+}
