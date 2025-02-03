e4ab2debf751bf540bea1c2beba114be0a659617
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-03 08:47:04 +0100

    Simplest for loop

---
+++ 06_zaklady/09_01_simplest_for_loop.go
@@ -0,0 +1,14 @@
+// Základy programovacího jazyka Go
+//
+// - nejjednodušší forma programové smyčky for
+
+package main
+
+import "fmt"
+
+func main() {
+	// nekonečná smyčka for
+	for {
+		fmt.Println("Diamonds are forever")
+	}
+}
