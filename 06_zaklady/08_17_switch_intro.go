1b9106e406f495fe6817a9533beface644116466
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-03 08:47:04 +0100

    Switch structure, basic usage

---
+++ 06_zaklady/08_17_switch_intro.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	switch {
+	}
+
+	switch {
+	default:
+		println("proč jsem vlastně použil switch?")
+	}
+
+	switch {
+	case true:
+		println("true")
+	case false:
+		println("false")
+	}
+
+	switch {
+	case false:
+		println("false")
+	case true:
+		println("true")
+	default:
+		println("default")
+	}
+
+	switch {
+	case false:
+		println("false")
+	default:
+		println("default")
+	case true:
+		println("true")
+	}
+}
