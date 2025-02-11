cf0a3444fd35149ad6f0f8bad6b025002d3dff42
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-11 09:15:50 +0100

    Array copy

---
+++ 08_slozene_datove_typy/02_13_array_copy.go
@@ -0,0 +1,30 @@
+// Pole v programovacím jazyku Go
+//
+// - kopie pole
+// - modifikace pole, ale nikoli jeho kopie
+
+package main
+
+import "fmt"
+
+func main() {
+	var a1 [10]int
+
+	// kopie pole
+	a2 := a1
+
+	// výpis obsahu obou polí
+	fmt.Printf("Array 1: %v\n", a1)
+	fmt.Printf("Array 2: %v\n", a2)
+
+	// modifikace pole a1
+	for i := 0; i < len(a1); i++ {
+		a1[i] = i * 2
+	}
+
+	fmt.Println("-------------------------------")
+
+	// výpis obsahu obou polí
+	fmt.Printf("Array 1: %v\n", a1)
+	fmt.Printf("Array 2: %v\n", a2)
+}
