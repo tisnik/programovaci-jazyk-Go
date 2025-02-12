bb3953e27ce88504f95cbaf92de1b66f816a9fc0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-12 12:53:11 +0100

    Statement evaluation in Yaegi interpreter

---
+++ 19_interpreters/03_02_yaegi_eval_2.go
@@ -0,0 +1,43 @@
+// Interpretry programovacího jazyka Go
+//
+// Interpret Yaegi
+//
+// - využití interpretru Yaegi
+// - vyhodnocení víceřadkového skriptu
+// - nevrací se (explicitně!) žádná hodnota
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+)
+
+// skript, který se bude interpretovat
+const script = `
+x:=6
+y:=7
+z:=x*y
+`
+
+// inicializace nového interpretru a vyhodnocení předaného skriptu
+func RunYaegi(script string) any {
+	// inicializace nového interpretru
+	interpreter := interp.New(interp.Options{})
+
+	interpreter.Use(stdlib.Symbols)
+
+	// vyhodnocení předaného skriptu
+	ret, err := interpreter.Eval(script)
+	if err != nil {
+		panic(err)
+	}
+
+	return ret
+}
+
+func main() {
+	fmt.Println(RunYaegi(script))
+}
