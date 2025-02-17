d19dcdd850d3e54623e077a99531fd9e87dc6498
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-17 09:19:52 +0100

    Updated comments

--- 06_zaklady/10_02_defer_func.go
+++ 06_zaklady/10_02_defer_func.go
@@ -1,10 +1,17 @@
+// Základy programovacího jazyka Go
+//
+// - anonymní funkce s odloženým voláním
+// - zápis této funkce v kulatých závorkách
+
 package main
 
 import "fmt"
 
 func main() {
+	// anonymní funkce s odloženým voláním
 	defer (func() { fmt.Println("Finished") })()
 
+	// simulace nějaké činnosti
 	for i := 10; i >= 0; i-- {
 		fmt.Printf("%2d\n", i)
 	}
