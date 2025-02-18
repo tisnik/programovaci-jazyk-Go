6b698be3a7014da7311a8e7bcb89f003a105bfd7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-18 09:40:17 +0100

    Updated comments

--- 06_zaklady/10_10_defer_practical_usage.go
+++ 06_zaklady/10_10_defer_practical_usage.go
@@ -1,3 +1,9 @@
+// Základy programovacího jazyka Go
+//
+// - praktické použití konstrukce defer
+// - realizace kopie obsahu souboru
+// - kratší varianta bez výpisů prováděných operací
+
 package main
 
 import (
@@ -7,21 +13,28 @@ import (
 )
 
 func copyFile(srcName, dstName string) (written int64, err error) {
+	// otevření zdrojového souboru pro čtení
 	src, err := os.Open(srcName)
 	if err != nil {
 		return
 	}
+	// zajistit uzavření souboru za všech okolností
 	defer src.Close()
 
+	// otevření cílového souboru pro zápis
 	dst, err := os.Create(dstName)
 	if err != nil {
 		return
 	}
+	// zajistit uzavření souboru za všech okolností
 	defer dst.Close()
 
+	// vlastní kopie
+	// (vrátí se počet zkopírovaných bajtů a chybová struktura)
 	return io.Copy(dst, src)
 }
 
+// otestování, zda kopie obsahu souboru pracuje korektně
 func testCopyFile(srcName, dstName string) {
 	copied, err := copyFile(srcName, dstName)
 	if err != nil {
@@ -33,9 +46,9 @@ func testCopyFile(srcName, dstName string) {
 }
 
 func main() {
-	testCopyFile("08_defer_practical_usage.go", "new.go")
+	testCopyFile("10_09_defer_practical_usage.go", "new.go")
 	testCopyFile("not_exists", "new.go")
-	testCopyFile("08_defer_practical_usage.go", "")
-	testCopyFile("08_defer_practical_usage.go", "/dev/full")
+	testCopyFile("10_09_defer_practical_usage.go", "")
+	testCopyFile("10_09_defer_practical_usage.go", "/dev/full")
 	testCopyFile("/dev/null", "new2.go")
 }
