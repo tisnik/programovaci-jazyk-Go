0bc5d4c229d83ecb9f459fccd011cc21ec19fa00
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-18 09:41:07 +0100

    Multiple defer statements in one function

---
+++ 06_zaklady/10_14_multiple_defers_return_value.go
@@ -0,0 +1,19 @@
+// Základy programovacího jazyka Go
+//
+// - manipulace s návratovýi hodnotami přímo v konstrukci defer
+
+package main
+
+import "fmt"
+
+func function(x int) (i int) {
+	// odložené nastavení návratové hodnoty
+	defer func() { i += 1 }()
+	defer func() { i *= 10 }()
+	// explicitní návrat s hodnotou 1
+	return x
+}
+
+func main() {
+	fmt.Printf("Return value of function: %d\n", function(100))
+}
