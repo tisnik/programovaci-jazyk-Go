3c50e157a07a1408381ec4631b9ee95267256a93
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-18 09:40:14 +0100

    Updated comments

--- 06_zaklady/10_09_defer_practical_usage.go
+++ 06_zaklady/10_09_defer_practical_usage.go
@@ -1,3 +1,9 @@
+// Základy programovacího jazyka Go
+//
+// - praktické použití konstrukce defer
+// - realizace kopie obsahu souboru
+// - delší varianta vypisující prováděné operace
+
 package main
 
 import (
@@ -6,31 +12,41 @@ import (
 	"os"
 )
 
+// pomocná funkce pro uzavření souboru
 func closeFile(file *os.File) {
 	fmt.Printf("Closing file '%s'\n", file.Name())
 	file.Close()
 }
 
 func copyFile(srcName, dstName string) (written int64, err error) {
+	// otevření zdrojového souboru pro čtení
 	src, err := os.Open(srcName)
 	if err != nil {
 		fmt.Printf("Cannot open file '%s' for reading\n", srcName)
 		return
 	}
 	fmt.Printf("File '%s' opened for reading\n", srcName)
+
+	// zajistit uzavření souboru za všech okolností
 	defer closeFile(src)
 
+	// otevření cílového souboru pro zápis
 	dst, err := os.Create(dstName)
 	if err != nil {
 		fmt.Printf("Cannot create destination file '%s'\n", dstName)
 		return
 	}
 	fmt.Printf("File '%s' opened for writing\n", dstName)
+
+	// zajistit uzavření souboru za všech okolností
 	defer closeFile(dst)
 
+	// vlastní kopie
+	// (vrátí se počet zkopírovaných bajtů a chybová struktura)
 	return io.Copy(dst, src)
 }
 
+// otestování, zda kopie obsahu souboru pracuje korektně
 func testCopyFile(srcName, dstName string) {
 	copied, err := copyFile(srcName, dstName)
 	if err != nil {
@@ -42,9 +58,9 @@ func testCopyFile(srcName, dstName string) {
 }
 
 func main() {
-	testCopyFile("08_defer_practical_usage.go", "new.go")
+	testCopyFile("10_09_defer_practical_usage.go", "new.go")
 	testCopyFile("not_exists", "new.go")
-	testCopyFile("08_defer_practical_usage.go", "")
-	testCopyFile("08_defer_practical_usage.go", "/dev/full")
+	testCopyFile("10_09_defer_practical_usage.go", "")
+	testCopyFile("10_09_defer_practical_usage.go", "/dev/full")
 	testCopyFile("/dev/null", "new2.go")
 }
