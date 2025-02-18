c3b9f463a5c8a81009ba06608bf0484d1050aef4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-18 09:40:06 +0100

    Updated comments

--- 06_zaklady/10_07_defer_arguments_evaluation.go
+++ 06_zaklady/10_07_defer_arguments_evaluation.go
@@ -1,3 +1,9 @@
+// Základy programovacího jazyka Go
+//
+// - ověření, kdy se vyhodnocují parametry předávané funkci
+//   s odloženým zavoláním
+// - předávání řezů
+
 package main
 
 import "fmt"
@@ -7,18 +13,31 @@ func function(a []int) {
 }
 
 func main() {
+	// proměnná, jejíž hodnota bude předáváná do funkce
 	var x = []int{1, 2, 3}
 	fmt.Printf("Current x value = %v\n", x)
+
+	// odložené zavolání funkce s předáním parametru
 	defer function(x)
 
+	// modifikace obsahu řezu
 	x[0] = 0
 	fmt.Printf("Current x value = %v\n", x)
+
+	// odložené zavolání funkce s předáním parametru
 	defer function(x)
 
+	// modifikace obsahu řezu
 	x[1] = 0
 	fmt.Printf("Current x value = %v\n", x)
+
+	// odložené zavolání funkce s předáním parametru
 	defer function(x)
 
+	// modifikace obsahu řezu
 	x[2] = 0
 	fmt.Printf("Current x value = %v\n", x)
+
+	// odložené zavolání funkce s předáním parametru
+	defer function(x)
 }
