6dd229534d01c0a9cb4400b83fca0854c942aac8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-19 09:02:43 +0100

    Type declaration in Gore interpreter

---
+++ 19_interpreters/02_08_gore_type_declaration.go
@@ -0,0 +1,23 @@
+// Interpretry programovacího jazyka Go
+//
+// Interpret Gore
+//
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po deklaraci uživatelského datového typu
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+
+package main
+
+import "github.com/k0kubun/pp/v3"
+
+func __gore_p(xs ...any) {
+    for _, x := range xs {
+        pp.Println(x)
+    }
+}
+func main() {}
+
+type User struct {
+    Name    string
+    Surname string
+}
