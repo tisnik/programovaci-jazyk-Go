6d96b00270d8f71793a3ade174a1a0d94c91a0b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-20 09:15:49 +0100

    Evaluation in Gomacro interpreter

---
+++ 19_interpreters/04_02_gomacro_eval_2.go
@@ -0,0 +1,39 @@
+// Interpretry programovacího jazyka Go
+//
+// Interpret Gomacro
+//
+// - využití interpretru Gomacro
+// - vyhodnocení víceřadkového skriptu
+// - nevrací se žádná hodnota
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/cosmos72/gomacro/fast"
+)
+
+// skript, který se bude interpretovat
+const script = `
+x:=6
+y:=7
+z:=x*y
+`
+
+// inicializace nového interpretru a vyhodnocení předaného skriptu
+func RunGomacro(script string) any {
+	// inicializace nového interpretru
+	interp := fast.New()
+
+	// vyhodnocení předaného skriptu
+	vals, _ := interp.Eval(script)
+	if len(vals) < 1 {
+		return "no value"
+	}
+	return vals[0].ReflectValue()
+}
+
+func main() {
+	fmt.Println(RunGomacro(script))
+}
