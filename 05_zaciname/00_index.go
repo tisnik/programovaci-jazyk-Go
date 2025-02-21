0ed3fc9b9ab7b49ddb95220264d495f3f6365a48
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:14:02 +0100

    Updated

--- 05_zaciname/00_index.go
+++ 05_zaciname/00_index.go
@@ -1,3 +1,12 @@
-
+// První kroky s Go
 //
 // Seznam příkladů uložených v tomto podadresáři:
+//
+// comments_1.go
+// comments_2.go
+// comments_3.go
+// comments_4.go
+// hello_world.go
+// minimal.go
+// multiple_imports_1.go
+// multiple_imports_2.go
