6371070ead961d80762b81ec546c4e37326f011c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:21:51 +0100

    Slice modification

---
+++ 08_slozene_datove_typy/03_02_modify_new_slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+
+	fmt.Printf("Type:  %T\n", s)
+
+	fmt.Printf("Before modification: %v\n", s)
+
+	s[0] = 100
+
+	fmt.Printf("After modification:  %v\n", s)
+}
