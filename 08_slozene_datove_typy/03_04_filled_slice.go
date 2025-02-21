1bdf9d4a47f5a583db282feee756e46d63f67e04
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:22:08 +0100

    Filled slice

---
+++ 08_slozene_datove_typy/03_04_filled_slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3}
+
+	fmt.Printf("Type:  %T\n", s)
+
+	fmt.Printf("Before modification: %v\n", s)
+
+	s[0] = 100
+
+	fmt.Printf("After modification:  %v\n", s)
+}
