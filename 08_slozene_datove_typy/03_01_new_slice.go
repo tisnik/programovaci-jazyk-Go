0f4eca9456b13dcbaee90a15adfc6c0b109bfd21
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-24 08:32:30 +0100

    Updated comments

--- 08_slozene_datove_typy/03_01_new_slice.go
+++ 08_slozene_datove_typy/03_01_new_slice.go
@@ -1,10 +1,16 @@
+// Řezy v programovacím jazyku Go
+//
+// - konstrukce takzvaného nulového řezu (nil slice)
+
 package main
 
 import "fmt"
 
 func main() {
+	// konstrukce nulového řezu
 	var s []int
 
+	// tisk hodnoty a typu řezu
 	fmt.Printf("Value: %v\n", s)
 	fmt.Printf("Type:  %T\n", s)
 }
