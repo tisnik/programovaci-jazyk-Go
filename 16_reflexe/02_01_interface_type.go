ab175055a382049fda7aae6dd786bfa6d00c0691
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-25 10:19:45 +0100

    Interface type

---
+++ 16_reflexe/02_01_interface_type.go
@@ -0,0 +1,63 @@
+// Reflexe v programovacím jazyku Go
+//
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// deklarace rozhraní s jedinou metodou
+type Shape interface {
+	area() float64
+}
+
+// deklarace uživatelského typu
+type Square struct {
+	sideLength float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (square Square) area() float64 {
+	return square.sideLength * square.sideLength
+}
+
+// deklarace uživatelského typu
+type Rectangle struct {
+	width, height float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (rectangle Rectangle) area() float64 {
+	return rectangle.width * rectangle.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (circle Circle) area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+// funkce akceptující libovolný typ splňující rozhraní Shape
+func displayArea(shape Shape) {
+	area := shape.area()
+	fmt.Println("Area: ", area)
+}
+
+func main() {
+	s := Square{sideLength: 10}
+	displayArea(s)
+
+	r := Rectangle{width: 3, height: 2}
+	displayArea(r)
+
+	c := Circle{radius: 1}
+	displayArea(c)
+}
