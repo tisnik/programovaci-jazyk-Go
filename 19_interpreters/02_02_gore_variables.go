f274ac29537e7af55b38a62f54dc7197cb9d891f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-25 10:19:26 +0100

    Updated format

--- 19_interpreters/02_02_gore_variables.go
+++ 19_interpreters/02_02_gore_variables.go
@@ -11,8 +11,8 @@ package main
 import "github.com/k0kubun/pp/v3"
 
 func __gore_p(xs ...any) {
-    for _, x := range xs {
-        pp.Println(x)
-    }
+	for _, x := range xs {
+		pp.Println(x)
+	}
 }
-func main() { x := 6; y := 7; z := "foobar" }
+func main() { x := 42; y := 10; z := "foobar" }
