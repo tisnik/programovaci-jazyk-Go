bb2a850f4593590c119061b35b29687bc6a20fa7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-25 10:20:10 +0100

    Type assertion with checks

---
+++ 16_reflexe/03_02_type_assertion_check.go
@@ -0,0 +1,42 @@
+// Reflexe v programovacím jazyku Go
+//
+// - deklarace rozhraní s jedinou metodou
+// - typová aserce
+// - použití nekorektního rozhraní
+
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// deklarace rozhraní s jedinou metodou
+type Shape interface {
+	area() float64
+}
+
+// deklarace uživatelského typu
+type Square struct {
+	sideLength float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (square Square) area() float64 {
+	return square.sideLength * square.sideLength
+}
+
+// funkce akceptující libovolný typ splňující rozhraní io.Writer
+// (Pozor: nikoli Shape)
+func displayShapeInfo(shape io.Writer) {
+	area := shape.area()
+	fmt.Println("Area: ", area)
+
+	square := shape.(Square)
+	fmt.Println("Side length: ", square.sideLength)
+}
+
+func main() {
+	s := Square{sideLength: 10}
+	displayShapeInfo(s)
+}
