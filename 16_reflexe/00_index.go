a3a3d15c0e41b1a0113d1dda4b66d228da2a6f35
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-26 09:44:38 +0100

    Updated list of examples

---
+++ 16_reflexe/00_index.go
@@ -0,0 +1,27 @@
+// Reflexe
+//
+// Seznam příkladů uložených v tomto podadresáři:
+// 00_index.go
+// 02_01_interface_type.go
+// 02_02_typecast.go
+// 03_01_type_assertion.go
+// 03_02_type_assertion_check.go
+// 03_03_type_assertion_check.go
+// 03_04_type_assertion_check.go
+// 03_05_type_assertion_example_1.go
+// 03_06_type_assertion_example_2.go
+// 03_07_type_assertion_example_3.go
+// 04_01_type_switch_1.go
+// 04_02_type_switch_2.go
+// 04_03_type_switch_3.go
+// 04_04_type_switch_4.go
+// 04_05_type_switch_5.go
+// 04_06_type_switch_6.go
+// 05_01_value_of.go
+// 05_02_value_of_type.go
+// 05_03_value_of_interface.go
+// 05_04_value_of_empty_interface.go
+// 05_05_values_of.go
+// 05_06_values_of_type.go
+// 06_01_values_of_nil.go
+// 06_02_values_of_type_nil.go
