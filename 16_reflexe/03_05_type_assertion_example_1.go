9109ac6c3bfbc9bc3af136b1dd3fe30621d27384
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-27 10:18:08 +0100

    Type assertion: example #1

---
+++ 16_reflexe/03_05_type_assertion_example_1.go
@@ -0,0 +1,44 @@
+// Reflexe v programovacím jazyku Go
+//
+// - typová aserce pro parametr typu any
+// - rozlišení mezi hodnotami typu int, bool a string
+
+package main
+
+import "fmt"
+
+func test_type(value any) {
+	int_val, ok := value.(int)
+	if ok {
+		fmt.Println("Integer value:", int_val)
+		return
+	}
+
+	bool_val, ok := value.(bool)
+	if ok {
+		fmt.Println("Boolean value:", bool_val)
+		return
+	}
+
+	string_val, ok := value.(string)
+	if ok {
+		fmt.Println("String value:", string_val)
+		return
+	}
+
+	fmt.Println("Unsupported value")
+}
+
+func main() {
+	x := 42
+	test_type(x)
+
+	y := true
+	test_type(y)
+
+	z := "foobar"
+	test_type(z)
+
+	w := 1 + 2i
+	test_type(w)
+}
