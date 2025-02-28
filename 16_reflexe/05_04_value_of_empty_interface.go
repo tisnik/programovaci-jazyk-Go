5743762ad286004bfcff4094797fb9021ccff4c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 09:47:25 +0100

    reflect.ValueOf function - return type for empty interface input

---
+++ 16_reflexe/05_04_value_of_empty_interface.go
@@ -0,0 +1,14 @@
+// Reflexe v programovacím jazyku Go
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var x interface{} = nil
+	v := reflect.ValueOf(x)
+	fmt.Printf("value %v of type %T\n", x, x)
+	fmt.Printf("value %v of type %T\n", v, v)
+}
