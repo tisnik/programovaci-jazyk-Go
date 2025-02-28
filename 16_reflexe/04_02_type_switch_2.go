81e66b4b3da871949d3641524cc7a9af5fa70c4a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 09:46:22 +0100

    Updated comments

--- 16_reflexe/04_02_type_switch_2.go
+++ 16_reflexe/04_02_type_switch_2.go
@@ -11,6 +11,9 @@ package main
 import "fmt"
 
 func test_type(value any) {
+	// konverze hodnoty předané v parametru typu "any"
+	// na vybrané konkrétní datové typy int, bool a string
+	// nakonec se provedou různé operace s konvertovanými hodnotami
 	switch v := value.(type) {
 	case int:
 		fmt.Println("Integer value:", v*10)
