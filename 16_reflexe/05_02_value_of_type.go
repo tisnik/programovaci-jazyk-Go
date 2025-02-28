bc5d75c5a6e58802aeaa55243f303496cd175693
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-28 09:47:03 +0100

    reflect.ValueOf function - return type

---
+++ 16_reflexe/05_02_value_of_type.go
@@ -0,0 +1,14 @@
+// Reflexe v programovacím jazyku Go
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	x := 42
+	v := reflect.ValueOf(x)
+	fmt.Printf("value %v of type %T\n", x, x)
+	fmt.Printf("value %v of type %T\n", v, v)
+}
