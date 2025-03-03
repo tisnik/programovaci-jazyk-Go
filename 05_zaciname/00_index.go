1d74f5c02df9bba95c0f3f01a220b5cdb12e640e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-03 08:59:43 +0100

    List of examples

--- 05_zaciname/00_index.go
+++ 05_zaciname/00_index.go
@@ -2,11 +2,28 @@
 //
 // Seznam příkladů uložených v tomto podadresáři:
 //
-// comments_1.go
-// comments_2.go
-// comments_3.go
-// comments_4.go
-// hello_world.go
-// minimal.go
-// multiple_imports_1.go
-// multiple_imports_2.go
+// comments_1.go:
+// - komentáře ve zdrojových kódech
+//
+// comments_2.go:
+// - víceřádkové komentáře ve zdrojových kódech
+//
+// comments_3.go:
+// - zakomentované části zdrojového kódu
+//
+// comments_4.go:
+// - zakomentované části zdrojového kódu
+//
+// hello_world.go:
+// - program typu "Hello, world!"
+//
+// minimal.go:
+// - nejkratší program vytvořený v jazyce Go
+//
+// multiple_imports_1.go:
+// - import symbolů ze dvou balíčků
+// - každý balíček je naimportován přes samostatný příkaz "import"
+//
+// multiple_imports_2.go:
+// - import symbolů ze dvou balíčků
+// - oba balíčky jsou naimportovány jediným příkazem "import"
