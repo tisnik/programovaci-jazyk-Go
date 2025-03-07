86cedf6b93e48aff9ad9291be9dc451fb3dc1d18
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-07 09:23:29 +0100

    value.Type() method

---
+++ 16_reflexe/07_02_get_type_interface.go
@@ -0,0 +1,33 @@
+// Reflexe v programovacím jazyku Go
+//
+//   - zjištění typu hodnoty uložené do proměnné
+//     typu any (prázdné rozhraní) s využitím
+//     funkce reflect.ValueOf
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	// získání informací o hodnotě v čase běhu aplikace
+	value := reflect.ValueOf(x)
+
+	// zjištění typu hodnoty
+	typ := value.Type()
+	fmt.Println("type is: ", typ)
+}
+
+// uživatelský datový typ
+type user struct {
+	name    string
+	surname string
+}
+
+func main() {
+	// proměnná je typu "any", ovšem obsahuje hodnotu typu "user"
+	var nil1 interface{} = user{name: "foo", surname: "bar"}
+	test_get_type(nil1)
+}
