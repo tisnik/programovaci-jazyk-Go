1a6504a09f21e1444fccd2031e6115f88a2abbc8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-07 09:23:29 +0100

    value.Type() method

---
+++ 16_reflexe/07_04_get_type_empty_interface.go
@@ -0,0 +1,26 @@
+// Reflexe v programovacím jazyku Go
+//
+// - pokus o zjištění hodnoty "nil" uložené
+//   v proměnné typu any (prázdné rozhraní)
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	// získání informací o hodnotě v čase běhu aplikace
+	value := reflect.ValueOf(x)
+
+	// zjištění typu hodnoty
+	typ := value.Type()
+	fmt.Println("type is: ", typ)
+}
+
+func main() {
+	// proměnná typu "any", která je inicializovaná na nulovou hodnotu
+	var nil1 interface{} = nil
+	test_get_type(nil1)
+}
