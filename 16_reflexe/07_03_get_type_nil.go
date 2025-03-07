a0a53c58a6aefae701cbdde76201647c9c24b80e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-07 09:23:29 +0100

    value.Type() method

---
+++ 16_reflexe/07_03_get_type_nil.go
@@ -0,0 +1,25 @@
+// Reflexe v programovacím jazyku Go
+//
+// - zjištění typu "nulového ukazatele"
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	// získání informací o hodnotě v čase běhu aplikace
+	value := reflect.ValueOf(x)
+
+	// zjištění typu hodnoty
+	typ := value.Type()
+	fmt.Println("type is: ", typ)
+}
+
+func main() {
+	// ukazatel na celé číslo, který obsahuje nulovou hodnotu
+	var nil1 *int = nil
+	test_get_type(nil1)
+}
