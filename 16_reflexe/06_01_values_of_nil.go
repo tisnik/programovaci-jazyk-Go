9b3dd4b14c0653a8de99d7e622286226139ec5f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-07 09:23:29 +0100

    Valued of type for nil values

---
+++ 16_reflexe/06_01_values_of_nil.go
@@ -0,0 +1,39 @@
+// Reflexe v programovacím jazyku Go
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func test_get_type(x any) {
+	value := reflect.ValueOf(x)
+	fmt.Println(x)
+	fmt.Println(value)
+	fmt.Println()
+}
+
+type user struct {
+	name    string
+	surname string
+}
+
+func main() {
+	var nil1 *int = nil
+	test_get_type(nil1)
+
+	var nil2 *bool = nil
+	test_get_type(nil2)
+
+	var nil3 *string = nil
+	test_get_type(nil3)
+
+	var nil4 *user = nil
+	test_get_type(nil4)
+
+	var nil5 interface{} = nil
+	test_get_type(nil5)
+
+	var nil6 []int = nil
+	test_get_type(nil6)
+}
