adbcb94675985125c36216b6455f81dd6d6aa9cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-10 08:41:14 +0100

    Updated comments

--- 19_interpreters/03_02_yaegi_eval_2.go
+++ 19_interpreters/03_02_yaegi_eval_2.go
@@ -4,6 +4,7 @@
 //
 // - využití interpretru Yaegi
 // - vyhodnocení víceřadkového skriptu
+// - skript je reprezentován víceřádkovým řetězcem
 // - nevrací se (explicitně!) žádná hodnota
 
 package main
