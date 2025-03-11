1dbe91766b1fde4b4a2d712d205b86f368a2f9d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-11 08:36:35 +0100

    Server sending text data

---
+++ 14_sitove_sluzby/01_07_text_server.go
@@ -0,0 +1,41 @@
+// Vývoj síťových aplikací v programovacím jazyku Go
+//
+// - standardní balíček net
+// - server posílající klientovi textová data
+
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// server bude naslouchat na portu 1234
+	l, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		fmt.Println("Can't open the port!")
+		return
+	}
+	defer l.Close()
+
+	// postupné zpracování požadavků od klientů
+	for {
+		// potvrzení připojení
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("connection refused!")
+		} else {
+			fmt.Println("connection accepted")
+			go func(c net.Conn) {
+				// poslat klientovi celý textový řádek
+				fmt.Fprintf(c, "Holla\n")
+				fmt.Println("sent whole text line")
+
+				// zajistit uzavření připojení
+				c.Close()
+			}(conn)
+		}
+	}
+}
