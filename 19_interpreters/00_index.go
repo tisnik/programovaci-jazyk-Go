35de048aa40443305fc854e4eb5d2226e0cffe1f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-14 10:40:24 +0100

    Updated index file

--- 19_interpreters/00_index.go
+++ 19_interpreters/00_index.go
@@ -1,4 +1,93 @@
 // Interpretry programovacího jazyka Go
 //
 // Seznam příkladů uložených v tomto podadresáři:
+//
+// 02_01_gore_init.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav ihned po inicializaci interpretru
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_02_gore_variables.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po deklaraci trojice proměnnych
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_03_gore_after_println.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po zadání příkazu println(x)
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_04_gore_after_println_2.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po zadání příkazů println(x) a println(y)
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_05_gore_after_import.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po zadání příkazu :import fmt
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_06_gore_autoimport.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - povolení automatických importů
+// - stav po zadání příkazu fmt.Println()
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_07_gore_function_declaration.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po deklaraci funkce add
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_08_gore_type_declaration.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po deklaraci uživatelského datového typu
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 02_09_gore_type_usage.go:
+// - zdrojový soubor vygenerovaný interpretrem Gore
+// - stav po deklaraci a použití uživatelského datového typu
+// - zdrojový kód je zobrazen přesně tak, jak ho tiskne pseudopříkaz :print
+//
+// 03_01_yaegi_eval_1.go:
+// - využití interpretru Yaegi
+// - vyhodnocení jednořádkového výrazu
+// - výraz je reprezentován řetězcem jazyka Go
+//
+// 03_02_yaegi_eval_2.go:
+// - využití interpretru Yaegi
+// - vyhodnocení víceřadkového skriptu
+// - skript je reprezentován víceřádkovým řetězcem
+// - nevrací se (explicitně!) žádná hodnota
+//
+// 03_03_yaegi_eval_3.go:
+// - využití interpretru Yaegi
+// - vyhodnocení víceřadkového skriptu
+// - skript je reprezentován víceřádkovým řetězcem
+// - skript vrací hodnotu
+//
+// 03_04_yaegi_eval_4.go:
+// - využití interpretru Yaegi
+// - vyhodnocení víceřadkového skriptu
+// - skript je reprezentován víceřádkovým řetězcem
+// - skript obsahuje deklaraci funkce
+//
+// 04_01_gomacro_eval_1.go:
+// - využití interpretru Gomacro
+// - vyhodnocení jednořádkového výrazu
+//
+// 04_02_gomacro_eval_2.go:
+// - využití interpretru Gomacro
+// - vyhodnocení víceřadkového skriptu
+// - nevrací se žádná hodnota
+//
+// 04_03_gomacro_eval_3.go:
+// - využití interpretru Gomacro
+// - vyhodnocení víceřadkového skriptu
+// - skript vrací hodnotu
+//
+// 04_04_gomacro_eval_4.go:
+// - využití interpretru Gomacro
+// - vyhodnocení víceřadkového skriptu
+// - skript obsahuje deklaraci funkce, která je volána
+//
 
