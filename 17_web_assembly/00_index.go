a238808adf67643c626beb44a96e8b63ea724368
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-14 10:40:04 +0100

    Updated index file

--- 17_web_assembly/00_index.go
+++ 17_web_assembly/00_index.go
@@ -1,3 +1,43 @@
 // Technologie WebAssembly
 //
 // Seznam příkladů uložených v tomto podadresáři:
+//
+// dom_add_element.go:
+// - manipulace s DOMem přímo z jazyka Go
+// - přidání nových elementů do HTML stránky
+//
+// dom_manipulation.go:
+// - manipulace s DOMem přímo z jazyka Go
+// - změna atributu vybraného elementu
+//
+// draw_into_canvas.go:
+// - manipulace s DOMem přímo z jazyka Go
+// - vytvoření kreslicího plátna (canvasu)
+// - kreslení na canvas
+//
+// hello_world.go:
+// - program typu "Hello, world!" ve variantě naprogramované
+//   v jazyku Go.
+// - importuje se standardní balíček "fmt" a volá se funkce fmt.Println
+// - tento program lze přeložit do nativního spustitelného
+//   souboru i do WebAssembly
+//
+// hello_world_println.go:
+// - program typu "Hello, world!" ve variantě naprogramované
+//   v jazyku Go.
+// - používá se pouze funkce println a neimportuje se žádný
+//   další balíček
+// - tento program lze přeložit do nativního spustitelného
+//   souboru i do WebAssembly
+//
+// http_server.go:
+// - implementace HTTP serveru, který dokáže na portu 8080 poskytnout
+//   všechny soubory umístěné v aktuálním adresáři
+// - souborům WASM bude automaticky přidělen korektní MIME typ
+//
+// js_interop_1.go:
+// - rozhraní mezi jazyky Go a JavaScript
+//
+// js_interop_2.go:
+// - rozhraní mezi jazyky Go a JavaScript
+//
