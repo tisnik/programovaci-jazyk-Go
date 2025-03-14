a76b05486a3c2ec315a5ee8278ae177d4ca0a0a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-14 10:40:10 +0100

    Updated index file

--- 18_gopherjs/00_index.go
+++ 18_gopherjs/00_index.go
@@ -1,35 +1,94 @@
 // Transpiler GopherJS
 //
 // Seznam příkladů uložených v tomto podadresáři:
-// 00_index.go
-// dom_add_element.go
-// dom_add_element.html
-// dom_manipulation.go
-// dom_manipulation.html
-// draw_into_canvas.go
-// draw_into_canvas.html
-// func_call_argument_types_1.go
-// func_call_argument_types_1.html
-// func_call_argument_types_2.go
-// func_call_argument_types_2.html
-// func_call_no_arguments.go
-// func_call_no_arguments.html
-// func_call_return_int_1.go
-// func_call_return_int_1.html
-// func_call_return_int_2.go
-// func_call_return_int_2.html
-// func_call_two_floats.go
-// func_call_two_floats.html
-// func_call_two_ints.go
-// func_call_two_ints.html
-// func_call_with_arguments.go
-// func_call_with_arguments.html
-// hello_world_2.go
-// hello_world_2.html
-// hello_world.go
-// hello_world.html
-// http_server.go
-// js_interop_1.go
-// js_interop_1.html
-// js_interop_2.go
-// js_interop_2.html
+//
+// callback.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - zavolání callback funkce
+//
+// dom_add_element.go:
+// - manipulace s DOMem přímo z jazyka Go
+// - přidání nových elementů do HTML stránky
+//
+// dom_manipulation.go:
+// - manipulace s DOMem přímo z jazyka Go
+// - změna atributu vybraného elementu
+//
+// draw_into_canvas.go:
+// - kresleni do canvasu
+//
+// func_call_argument_types_1.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - metoda PrintArguments vytiskne typy svých argumentů
+//
+// func_call_argument_types_1_webassembly.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - metoda PrintArguments vytiskne typy svých argumentů
+//
+// func_call_argument_types_2.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - metoda PrintArguments vytiskne typy svých argumentů
+// - lepší řešení založené na typu js.Type
+//
+// func_call_array_1.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintArrayLength
+// - kontrola typu argumentů předaných funkci PrintArrayLength
+// - výpočet délky pole
+//
+// func_call_array_2.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintArray
+// - kontrola typu argumentů předaných funkci PrintArray
+// - výpočet délky pole
+// - výpis prvků pole
+//
+// func_call_array_sum.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci arraySum
+// - kontrola typu argumentů předaných funkci arraySum
+// - výpočet délky pole
+// - výpočet součtu prvků pole
+//
+// func_call_no_arguments.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - funkce PrintHello neočekává resp. nezpracovává žadné argumenty
+//
+// func_call_no_arguments_webassembly.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - funkce PrintHello neočekává resp. nezpracovává žadné argumenty
+//
+// func_call_object_1.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintObject
+// - kontrola typu argumentů předaných funkci PrintObject
+//
+// func_call_object_2.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintObject
+// - kontrola typu argumentů předaných funkci PrintObject
+//
+// func_call_object_3.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintObject
+// - kontrola typu argumentů předaných funkci PrintObject
+//
+// func_call_return_int_1.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci CalcSum
+// - kontrola typu argumentů předaných funkci CalcSum
+// - provedení konverze na nativní typy jazyka Go
+// - převedení výsledku zpět na JavaScriptový typ
+//
+// func_call_return_int_2.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci CalcSum
+// - kontrola typu argumentů předaných funkci CalcSum
+// - provedení konverze na nativní typy jazyka Go
+// - automatická konverze výsledku zpět na JavaScriptový typ
+//
+// func_call_string.go:
+// - rozhraní mezi jazyky Go a JavaScript
+// - kontrola počtu argumentů předaných funkci PrintMessage
+// - kontrola typu argumentů předaných funkci PrintMessage
+// - provedení konverze na nativní typy jazyka Go
