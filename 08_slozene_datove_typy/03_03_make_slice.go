64eaed26088857733c810011b173230f104df628
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-18 09:26:58 +0100

    Updated comments

--- 08_slozene_datove_typy/03_03_make_slice.go
+++ 08_slozene_datove_typy/03_03_make_slice.go
@@ -12,6 +12,9 @@ func main() {
 	s := make([]int, 0)
 
 	// tisk hodnoty a typu řezu
-	fmt.Printf("Value: %v\n", s)
-	fmt.Printf("Type:  %T\n", s)
+	fmt.Printf("Hodnota: %v\n", s)
+	fmt.Printf("Typ:     %T\n", s)
+	fmt.Printf("=nil?    %v\n", s == nil)
+	fmt.Printf("Len:     %d\n", len(s))
+	fmt.Printf("Cap:     %d\n", cap(s))
 }
