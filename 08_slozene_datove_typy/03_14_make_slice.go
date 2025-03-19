12bc005499120d287ba7df602290a3820bb885ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-19 10:50:55 +0100

    Make slice

---
+++ 08_slozene_datove_typy/03_14_make_slice.go
@@ -0,0 +1,20 @@
+// Řezy v programovacím jazyku Go
+//
+// - konstrukce prázdného řezu pomocí make
+// - řez bude mít nenulovou kapacitu
+
+package main
+
+import "fmt"
+
+func main() {
+	// konstrukce prázdného řezu
+	s := make([]int, 0, 10)
+
+	// tisk hodnoty a typu řezu
+	fmt.Printf("Hodnota: %v\n", s)
+	fmt.Printf("Typ:     %T\n", s)
+	fmt.Printf("=nil?    %v\n", s == nil)
+	fmt.Printf("Len:     %d\n", len(s))
+	fmt.Printf("Cap:     %d\n", cap(s))
+}
