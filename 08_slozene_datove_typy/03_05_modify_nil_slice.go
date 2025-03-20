f7ca00adae1fae997124b1a7707d098ee3cdd49c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:05:21 +0100

    Updated comments

--- 08_slozene_datove_typy/03_05_modify_nil_slice.go
+++ 08_slozene_datove_typy/03_05_modify_nil_slice.go
@@ -1,15 +1,21 @@
 // Řezy v programovacím jazyku Go
+//
+// - konstrukce nulového řezu
+// - pokus o modifikaci prvního prvku, který neexistuje
+
 package main
 
 import "fmt"
 
 func main() {
+	// konstrukce nulového řezu
 	var s []int
 
 	fmt.Printf("Typ:  %T\n", s)
 
 	fmt.Printf("Před modifikací: %v\n", s)
 
+	// modifikace prvku, který neexistuje
 	s[0] = 100
 
 	fmt.Printf("Po modifikaci:   %v\n", s)
