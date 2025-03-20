11c40afaa8c3353b9716fddf398ba08be126d9bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:05:21 +0100

    Updated comments

--- 08_slozene_datove_typy/03_06_modify_filled_slice.go
+++ 08_slozene_datove_typy/03_06_modify_filled_slice.go
@@ -1,15 +1,21 @@
 // Řezy v programovacím jazyku Go
+//
+// - konstrukce řezu s jeho inicializací
+// - modifikace prvního prvku uloženého v řezu
+
 package main
 
 import "fmt"
 
 func main() {
+	// konstrukce řezu s jeho inicializací
 	s := []int{1, 2, 3}
 
 	fmt.Printf("Typ:  %T\n", s)
 
 	fmt.Printf("Před modifikací: %v\n", s)
 
+	// modifikace prvku
 	s[0] = 100
 
 	fmt.Printf("Po modifikaci:   %v\n", s)
