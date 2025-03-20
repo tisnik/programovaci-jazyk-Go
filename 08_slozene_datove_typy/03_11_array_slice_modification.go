c96bbc2dde018fac588574254baef2fa0d069f17
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:05:22 +0100

    Updated comments

--- 08_slozene_datove_typy/03_11_array_slice_modification.go
+++ 08_slozene_datove_typy/03_11_array_slice_modification.go
@@ -1,17 +1,24 @@
 // Řezy v programovacím jazyku Go
+//
+// - modifikace řezu
+// - zjištění, jak se změní pole, na které řez ukazuje
+
 package main
 
 import "fmt"
 
 func main() {
+	// pole
 	var a [10]int
 
+	// vytvoření řezu z pole
 	slice := a[:]
 
 	fmt.Printf("Pole před modifikací: %v\n", a)
 	fmt.Printf("Řez před modifikací:  %v\n", slice)
 	fmt.Println()
 
+	// modifikace pole
 	for i := 0; i < len(a); i++ {
 		a[i] = i * 2
 	}
@@ -20,6 +27,7 @@ func main() {
 	fmt.Printf("Řez po modifikaci:    %v\n", slice)
 	fmt.Println()
 
+	// modifikace řezu
 	for i := 0; i < len(slice); i++ {
 		slice[i] = 42
 	}
