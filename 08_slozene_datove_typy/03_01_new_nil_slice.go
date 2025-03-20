6da0f919bd36a09e298a21c9bb3dc32760cf1e10
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:05:21 +0100

    Updated comments

--- 08_slozene_datove_typy/03_01_new_nil_slice.go
+++ 08_slozene_datove_typy/03_01_new_nil_slice.go
@@ -14,4 +14,6 @@ func main() {
 	fmt.Printf("Hodnota: %v\n", s)
 	fmt.Printf("Typ:     %T\n", s)
 	fmt.Printf("=nil?    %v\n", s == nil)
+	fmt.Printf("Len:     %d\n", len(s))
+	fmt.Printf("Cap:     %d\n", cap(s))
 }
