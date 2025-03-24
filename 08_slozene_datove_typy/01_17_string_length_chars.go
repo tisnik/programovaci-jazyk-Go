0fd657e1e6afd5e7f3817e9b208b90d2636651ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-24 08:09:25 +0100

    Updated comments

--- 08_slozene_datove_typy/01_17_string_length_chars.go
+++ 08_slozene_datove_typy/01_17_string_length_chars.go
@@ -9,12 +9,14 @@ import (
 )
 
 func main() {
+	// deklarace různých řetězců
 	var s1 string
 	var s2 string = ""
 	var s3 string = "Hello world!"
 	var s4 string = "ěščřžýáíéů"
 	var s5 string = "шщэюя"
 
+	// tisk délek řetězců v runách
 	fmt.Println(len([]rune(s1)))
 	fmt.Println(len([]rune(s2)))
 	fmt.Println(len([]rune(s3)))
