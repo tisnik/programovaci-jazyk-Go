17dc858a318edcaaa0ff80e9f135c0c7b2b30ce7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-25 08:20:37 +0100

    Slice modifications

---
+++ 08_slozene_datove_typy/03_15_slice_modification.go
@@ -0,0 +1,39 @@
+// Řezy v programovacím jazyku Go
+//
+// - vytvoření řezu z jiného řezu
+// - zjištění, jak se modifikace prvku v jednom řezu
+//   projeví na druhém řezu
+
+package main
+
+import "fmt"
+
+func main() {
+	// pole
+	a := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// řez vytvořený z pole (řez polem)
+	slice1 := a[4:9]
+
+	// řez vytvořený z jiného řezu (řez řezem)
+	slice2 := slice1[3:]
+
+	fmt.Printf("Pole:            %v\n", a)
+	fmt.Printf("Délka pole:      %d\n\n", len(a))
+
+	fmt.Printf("Řez 1:           %v\n", slice1)
+	fmt.Printf("Délka řezu 1:    %d\n", len(slice1))
+	fmt.Printf("Kapacita řezu 1: %d\n\n", cap(slice1))
+
+	fmt.Printf("Řez 2:           %v\n", slice2)
+	fmt.Printf("Délka řezu 2:    %d\n", len(slice2))
+	fmt.Printf("Kapacita řezu 2: %d\n\n", cap(slice2))
+
+	// modifikace prvků přes druhý řez
+	slice2[0] = 99
+	slice2[1] = 99
+
+	fmt.Printf("Pole:            %v\n", a)
+	fmt.Printf("Řez 1:           %v\n", slice1)
+	fmt.Printf("Řez 2:           %v\n", slice2)
+}
