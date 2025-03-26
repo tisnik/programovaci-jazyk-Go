4cb8d9ea2c57d45625062adde5fa655e154f03fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-26 08:55:01 +0100

    Index file

---
+++ 09_ukazatele/00_index.go
@@ -0,0 +1,13 @@
+// Ukazatele
+//
+// Seznam příkladů uložených v tomto podadresáři:
+// 01_01_pointer_variable.go
+// 01_02_pointer_variable_type.go
+// 01_03_pointers_zero_values.go
+// 01_04_standard_types_print.go
+// 01_05_types_and_zero_values.go
+// 01_06_pointer_usage.go
+// 01_07_pointer_usage.go
+// 01_08_pointer_usage.go
+// 01_09_pass_pointer.go
+// 01_10_nil_pointer.go
