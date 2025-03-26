b1106357ebb891edaf794fa5df8745f121f98997
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-26 08:53:34 +0100

    HTML templates

---
+++ 14_sitove_sluzby/03_01_html_templates.go
@@ -0,0 +1,42 @@
+// Vývoj síťových aplikací v programovacím jazyku Go
+//
+// - standardní balíček net/http
+// - základní použití šablon
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// IndexPageDynContent holds all required information for a main page template
+type IndexPageDynContent struct {
+	Title  string
+	Header string
+}
+
+func mainEndpoint(writer http.ResponseWriter, request *http.Request) {
+	t, err := template.ParseFiles("index_template.html")
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+		return
+	}
+
+	dynData := IndexPageDynContent{Title: "Test", Header: "Welcome!"}
+	err = t.Execute(writer, dynData)
+	if err != nil {
+		fmt.Println("Error executing template")
+	}
+}
+
+func main() {
+	// registrace callback funkce volané při přístupu na endpoint
+	http.HandleFunc("/", mainEndpoint)
+
+	// spuštění HTTP serveru
+	fmt.Println("Starting HTTP server")
+	http.ListenAndServe(":8000", nil)
+}
