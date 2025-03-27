556259d9fe3c6d981696812aa807c8545eed3537
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-27 08:36:18 +0100

    Better struct init

---
+++ 08_slozene_datove_typy/05_03_better_struct_init.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// User je uživatelsky definovaná datová struktura
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	user1 := User{
+		id:      1,
+		name:    "Pepek",
+		surname: "Vyskoč"}
+
+	fmt.Println(user1)
+
+	user1.id = 2
+	user1.name = "Josef"
+	user1.surname = "Vyskočil"
+
+	fmt.Println(user1)
+}
