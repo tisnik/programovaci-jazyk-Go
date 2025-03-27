c5590269f94596c2b0afeeb0ea591cc6817051ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-27 08:37:02 +0100

    Map of struct

---
+++ 08_slozene_datove_typy/05_08_map_and_struct.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Key je uživatelsky definovaná datová struktura
+type Key struct {
+	id   uint32
+	role string
+}
+
+// User je uživatelsky definovaná datová struktura
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	m1 := make(map[Key]User)
+	fmt.Println(m1)
+
+	m1[Key{1, "admin"}] = User{
+		id:      1,
+		name:    "Pepek",
+		surname: "Vyskoč"}
+
+	m1[Key{2, "user"}] = User{
+		id:      2,
+		name:    "Josef",
+		surname: "Vyskočil"}
+
+	fmt.Println(m1)
+}
