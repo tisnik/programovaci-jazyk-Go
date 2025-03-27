c47aa8895a66fc29d20cabc130d5f3843651ad2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-27 08:36:49 +0100

    Array of structs

---
+++ 08_slozene_datove_typy/05_06_array_of_structs.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// User je uživatelsky definovaná datová struktura
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	var users = [3]User{
+		User{
+			id:      1,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      2,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      3,
+			name:    "Josef",
+			surname: "Vyskočil"},
+	}
+	fmt.Println(users)
+
+	var users2 = [...]User{
+		User{
+			id:      1,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      2,
+			name:    "Pepek",
+			surname: "Vyskoč"},
+		User{
+			id:      3,
+			name:    "Josef",
+			surname: "Vyskočil"},
+	}
+	fmt.Println(users2)
+}
