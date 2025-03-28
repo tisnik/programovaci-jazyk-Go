f6172e99a09b9b63ca1b36275f9e19fc527bf6ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 08:24:12 +0100

    Variable of type pointer

---
+++ 09_ukazatele/01_01_pointer_variable.go
@@ -0,0 +1,16 @@
+// Ukazatele
+//
+// - deklarace proměnné typu ukazatel
+// - tisk nulové hodnoty této proměnné
+
+package main
+
+import "fmt"
+
+func main() {
+	// deklarace proměnné typu ukazatel
+	var p *int
+
+	// tisk hodnoty ukazatele
+	fmt.Println(p)
+}
