3d5522004b55345eaf3def0e26cd02025d3f20f7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-31 09:43:00 +0200

    Updated comments

--- 09_ukazatele/01_07_pointer_usage.go
+++ 09_ukazatele/01_07_pointer_usage.go
@@ -1,4 +1,4 @@
-// Ukazatele
+// Ukazatele v jazyku Go
 //
 // - deklarace proměnné typu ukazatel
 // - nastavení proměnné přes ukazatel
@@ -8,12 +8,19 @@ package main
 import "fmt"
 
 func main() {
+	// běžná proměnná
 	var x int = 42
+
+	// ukazatel inicializovaný na adresu proměnné
 	var p *int = &x
 
+	// tisk hodnoty proměnné i ukazatele
 	fmt.Println("x=", x)
 	fmt.Println("p=", p)
 
+	// změna proměnné x nepřímo - přes ukazatel
 	*p = -1
+
+	// tisk nové hodnoty proměnné
 	fmt.Println("x=", x)
 }
