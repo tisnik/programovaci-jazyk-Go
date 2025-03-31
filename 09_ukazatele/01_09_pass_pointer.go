7bee6b5fc23cd44295fe216fb7916a90e14a838f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-31 09:43:07 +0200

    Updated comments

--- 09_ukazatele/01_09_pass_pointer.go
+++ 09_ukazatele/01_09_pass_pointer.go
@@ -1,4 +1,4 @@
-// Ukazatele
+// Ukazatele v jazyku Go
 //
 // - deklarace proměnné typu ukazatel
 // - nastavení proměnné přes ukazatel
@@ -8,14 +8,22 @@ package main
 
 import "fmt"
 
+// funkce, která modifikuje obsah proměnné přes ukazatel
 func increment(ptr *int) {
+	// nepřímá změna obsahu proměnné
 	*ptr++
 }
 
 func main() {
+	// deklarace proměnné s její inicializací
 	x := 42
+
+	// tisk původního obsahu proměnné
 	fmt.Println("x=", x)
 
+	// modifikace proměnné nepřímo přes ukazatel
 	increment(&x)
+
+	// tisk nového obsahu proměnné
 	fmt.Println("x=", x)
 }
