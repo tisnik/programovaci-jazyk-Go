40ed415eb7a53d62d89dab019f2abf9f17a91e57
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-31 09:43:13 +0200

    Updated comments

--- 09_ukazatele/01_10_nil_pointer.go
+++ 09_ukazatele/01_10_nil_pointer.go
@@ -1,4 +1,4 @@
-// Ukazatele
+// Ukazatele v jazyku Go
 //
 // - deklarace proměnné typu ukazatel
 // - nastavení proměnné přes ukazatel
@@ -6,12 +6,17 @@
 
 package main
 
+// funkce, která modifikuje obsah proměnné přes ukazatel
 func increment(ptr *int) {
+	// nepřímá změna obsahu proměnné
 	*ptr++
 }
 
 func main() {
+	// deklarace ukazatele, který bude nastavený na nil
 	var p *int
 
+	// pokus o zvýšení hodnoty uložené na adrese, kterou
+	// obsahuje ukazatel
 	increment(p)
 }
