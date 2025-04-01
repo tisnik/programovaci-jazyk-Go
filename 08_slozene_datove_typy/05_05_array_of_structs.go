73f3231c7b257000dbfdf55a11782247f14c03fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-01 09:57:51 +0200

    Updated comments

--- 08_slozene_datove_typy/05_05_array_of_structs.go
+++ 08_slozene_datove_typy/05_05_array_of_structs.go
@@ -1,3 +1,7 @@
+// Struktury (záznamy) v programovacím jazyku Go
+//
+// - pole, jehož prvky jsou typu struktura (záznam)
+
 package main
 
 import "fmt"
@@ -10,26 +14,35 @@ type User struct {
 }
 
 func main() {
+	// pole záznamů
 	var users [3]User
+
+	// tisk obsahu celého pole
 	fmt.Println(users)
 
+	// první prvek, který zapíšeme do pole
 	user1 := User{
 		id:      1,
 		name:    "Pepek",
 		surname: "Vyskoč"}
 
+	// druhý prvek, který zapíšeme do pole
 	var user2 User
 	user2.id = 1
 	user2.name = "Pepek"
 	user2.surname = "Vyskoč"
 
+	// třetí prvek, který zapíšeme do pole
 	user3 := User{
 		id:      1,
 		name:    "Josef",
 		surname: "Vyskočil"}
 
+	// změna obsahu prvků pole
 	users[0] = user1
 	users[1] = user2
 	users[2] = user3
+
+	// tisk obsahu celého pole
 	fmt.Println(users)
 }
