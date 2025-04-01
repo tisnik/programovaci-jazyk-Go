468d46f0c997b4176141f864d15e01c894feac94
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-01 09:57:41 +0200

    Updated comments

--- 08_slozene_datove_typy/05_02_struct_init.go
+++ 08_slozene_datove_typy/05_02_struct_init.go
@@ -1,3 +1,8 @@
+// Struktury (záznamy) v programovacím jazyku Go
+//
+// - inicializace prvků datové struktury
+// - zápis inicializace odvozený od jazyka C
+
 package main
 
 import "fmt"
@@ -10,16 +15,20 @@ type User struct {
 }
 
 func main() {
+	// deklarace proměnné s její inicializací
 	user1 := User{
 		1,
 		"Pepek",
 		"Vyskoč"}
 
+	// tisk obsahu celé struktury
 	fmt.Println(user1)
 
+	// zápis hodnot prvků do datové struktury
 	user1.id = 2
 	user1.name = "Josef"
 	user1.surname = "Vyskočil"
 
+	// tisk obsahu celé struktury
 	fmt.Println(user1)
 }
