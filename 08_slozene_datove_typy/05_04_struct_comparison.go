d4d7064d0535fefcfd8f5cd20cf1fc0596dd541d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-01 09:57:47 +0200

    Updated comments

--- 08_slozene_datove_typy/05_04_struct_comparison.go
+++ 08_slozene_datove_typy/05_04_struct_comparison.go
@@ -1,3 +1,9 @@
+// Struktury (záznamy) v programovacím jazyku Go
+//
+// - inicializace prvků datové struktury
+// - vylepšený zápis inicializace s vyjmenováním prvků
+// - porovnání dvou struktur
+
 package main
 
 import "fmt"
@@ -10,21 +16,26 @@ type User struct {
 }
 
 func main() {
+	// deklarace proměnné s její inicializací
+	// s vyjmenováním nastavovaných prvků
 	user1 := User{
 		id:      1,
 		name:    "Pepek",
 		surname: "Vyskoč"}
 
+	// deklarace druhé proměnné typu záznam
 	var user2 User
 	user2.id = 1
 	user2.name = "Pepek"
 	user2.surname = "Vyskoč"
 
+	// deklarace třetí proměnné typu záznam
 	user3 := User{
 		id:      1,
 		name:    "Josef",
 		surname: "Vyskočil"}
 
+	// záznamy (struktury) je možné porovnat operátorem ==
 	fmt.Println(user1 == user2)
 	fmt.Println(user1 == user3)
 }
