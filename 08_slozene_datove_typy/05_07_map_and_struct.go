139be39a739dd4772c19e4d99f680c8ac0fc7686
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-01 09:57:57 +0200

    Updated comments

--- 08_slozene_datove_typy/05_07_map_and_struct.go
+++ 08_slozene_datove_typy/05_07_map_and_struct.go
@@ -1,3 +1,8 @@
+// Struktury (záznamy) v programovacím jazyku Go
+//
+// - mapa, která obsahuje uložené záznamy
+// - jako klíče jsou použity řetězce
+
 package main
 
 import "fmt"
@@ -10,18 +15,24 @@ type User struct {
 }
 
 func main() {
+	// inicializace prázdné mapy
 	m1 := make(map[string]User)
+
+	// tisk prázdné mapy
 	fmt.Println(m1)
 
+	// přidání prvku do mapy
 	m1["prvni"] = User{
 		id:      1,
 		name:    "Pepek",
 		surname: "Vyskoč"}
 
+	// přidání prvku do mapy
 	m1["druhy"] = User{
 		id:      2,
 		name:    "Josef",
 		surname: "Vyskočil"}
 
+	// tisk naplněné mapy
 	fmt.Println(m1)
 }
