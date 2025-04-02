9be1a41554ae27008356c568570a516d088dbbea
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-02 10:23:19 +0200

    Updated index file

--- 09_ukazatele/00_index.go
+++ 09_ukazatele/00_index.go
@@ -1,13 +1,70 @@
 // Ukazatele
 //
 // Seznam příkladů uložených v tomto podadresáři:
-// 01_01_pointer_variable.go
-// 01_02_pointer_variable_type.go
-// 01_03_pointers_zero_values.go
-// 01_04_standard_types_print.go
-// 01_05_types_and_zero_values.go
-// 01_06_pointer_usage.go
-// 01_07_pointer_usage.go
-// 01_08_pointer_usage.go
-// 01_09_pass_pointer.go
-// 01_10_nil_pointer.go
+//
+// 01_01_pointer_variable.go:
+// - deklarace lokální proměnné typu ukazatel
+// - tisk nulové hodnoty této proměnné
+//
+// 01_02_pointer_variable_type.go:
+// - deklarace lokální proměnné typu ukazatel
+// - tisk typu této proměnné
+//
+// 01_03_pointers_zero_values.go:
+// - deklarace proměnných typu ukazatel, z nichž každý
+//   je odlišného základního datového typu (nebo jeho aliasu)
+// - tisk výchozích (nulových) hodnot těchto proměnných
+//
+// 01_04_standard_types_print.go:
+// - deklarace proměnných typu ukazatel, z nichž každý
+//   je odlišného základního datového typu (nebo jeho aliasu)
+// - tisk jmen typů proměnných
+//
+// 01_05_types_and_zero_values.go:
+// - deklarace proměnných typu ukazatel, z nichž každý
+//   je odlišného základního datového typu (nebo jeho aliasu)
+// - tisk jmen typů proměnných i jejich (nulových) hodnot
+//
+// 01_06_pointer_usage.go:
+// - deklarace proměnné typu ukazatel
+// - nastavení proměnné přes ukazatel
+//
+// 01_07_pointer_usage.go:
+// - deklarace proměnné typu ukazatel
+// - nastavení proměnné přes ukazatel
+//
+// 01_08_pointer_usage.go:
+// - deklarace proměnné typu ukazatel
+// - nastavení proměnné přes ukazatel
+// - použití typové inference
+//
+// 01_09_pass_pointer.go:
+// - deklarace proměnné typu ukazatel
+// - nastavení proměnné přes ukazatel
+// - použití typové inference
+//
+// 01_10_nil_pointer.go:
+// - deklarace proměnné typu ukazatel
+// - nastavení proměnné přes ukazatel
+// - použití typové inference
+//
+// 01_11_return_pointer.go:
+// - funkce vracející ukazatel na lokální proměnnou
+//
+// 01_12_pointer_to_struct.go:
+// - ukazatel na záznam (strukturu)
+// - změna prvku záznamu nepřímo přes ukazatel
+//
+// 01_13_pointer_to_struct_item.go:
+// - ukazatele na prvky záznamu
+//
+// 01_14_pointer_to_array_item.go:
+// - ukazatel na prvek pole
+//
+// 01_15_pointer_to_pointer.go:
+// - ukazatel na jiný ukazatel
+//
+// 01_16_flags.go:
+// - praktické použití ukazatelů
+// - přečtení argumentů předaných na příkazovém řádku
+
