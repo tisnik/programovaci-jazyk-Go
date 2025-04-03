b04abfca7a583e1f880f23053a77c5d60333842f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-03 10:30:06 +0200

    Shapes implementation

---
+++ 11_oop/03_03_shapes_3.go
@@ -0,0 +1,39 @@
+// Koncept rozhraní v programovacím jazyku Go
+//
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelského typu
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotou, která rozhraní Shape nesplňuje
+
+package main
+
+import (
+	"fmt"
+)
+
+// deklarace rozhraní s jedinou metodou
+type Shape interface {
+	area() float64
+}
+
+// deklarace uživatelského typu
+type Square struct {
+	sideLength float64
+}
+
+// zdánlivá implementace metody předepsané v rozhraní Shape
+// ovšem s nekorektním návratovým typem
+func (square Square) area() float32 {
+	return float32(square.sideLength * square.sideLength)
+}
+
+// funkce akceptující libovolný typ splňující rozhraní Shape
+func displayArea(shape Shape) {
+	area := shape.area()
+	fmt.Println("Area: ", area)
+}
+
+func main() {
+	s := Square{sideLength: 10}
+	displayArea(s)
+}
