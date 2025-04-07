4361816110069d92ad2e500e0f0d7a7de4a508a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-07 09:54:23 +0200

    Updated comments

--- 08_slozene_datove_typy/02_01_array_global_variable.go
+++ 08_slozene_datove_typy/02_01_array_global_variable.go
@@ -1,11 +1,12 @@
 // Pole v programovacím jazyku Go
 //
-// - deklarace globální konstanty s polem
+// - deklarace globální proměnné s polem
 
 package main
 
 import "fmt"
 
+// globální proměnná s polem
 var a1 [10]int
 
 func main() {
