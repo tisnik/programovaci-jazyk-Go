f1fd4f191c902a0a8b30accb3f31a7e234227317
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-07 09:54:23 +0200

    Updated comments

--- 08_slozene_datove_typy/02_04_array_initialization.go
+++ 08_slozene_datove_typy/02_04_array_initialization.go
@@ -1,13 +1,16 @@
 // Pole v programovacím jazyku Go
 //
-// - deklarace lokální konstanty s polem
+// - deklarace lokální proměnné s polem
 // - inicializace pole
+// - počet prvků si zjistí překladač automaticky
 
 package main
 
 import "fmt"
 
 func main() {
+	// deklarace lokální proměnné s polem
+	// inicializace prvků
 	var a1 [10]int = [...]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
 
 	// tisk pole
