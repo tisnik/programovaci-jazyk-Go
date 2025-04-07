7928447d5912d9788ae0bf9b59f0b2750a07b9b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-07 09:54:23 +0200

    Updated comments

--- 08_slozene_datove_typy/02_02_array_local_variable.go
+++ 08_slozene_datove_typy/02_02_array_local_variable.go
@@ -1,12 +1,13 @@
 // Pole v programovacím jazyku Go
 //
-// - deklarace lokální konstanty s polem
+// - deklarace lokální proměnné s polem
 
 package main
 
 import "fmt"
 
 func main() {
+	// lokální proměnná s polem
 	var a1 [10]int
 
 	// tisk lokálního pole
