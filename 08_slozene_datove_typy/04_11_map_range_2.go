70e9b34f1e00dad37f03148f37394c0830d9348c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-08 09:31:46 +0200

    For-range loop for map

---
+++ 08_slozene_datove_typy/04_11_map_range_2.go
@@ -0,0 +1,24 @@
+// Mapy v programovacím jazyku Go
+//
+// - průchod všemi hodnotami prvků mapy
+
+package main
+
+import "fmt"
+
+func main() {
+	m1 := make(map[string]int)
+
+	m1["nula"] = 0
+	m1["jedna"] = 1
+	m1["dva"] = 2
+	m1["tri"] = 3
+	m1["ctyri"] = 4
+	m1["pet"] = 5
+	m1["sest"] = 6
+
+	// průchod všemi hodnotami prvků mapy
+	for _, value := range m1 {
+		fmt.Printf("%d\n", value)
+	}
+}
