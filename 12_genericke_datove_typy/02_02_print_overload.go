f84fabe5d7c84eec9ac27811d2d27ed1878137c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-09 09:37:37 +0200

    Print funtion overloaded: incorrect

---
+++ 12_genericke_datove_typy/02_02_print_overload.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func printValue(value string) {
+	fmt.Println(value)
+}
+
+func printValue(value int) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue("Učíme se jazyk Go!")
+}
