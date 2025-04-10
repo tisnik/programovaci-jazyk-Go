0161c7f0887c38a1b5ef990a5299652e74b7c662
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-10 09:20:54 +0200

    Updated comments

--- 12_genericke_datove_typy/02_02_print_overload.go
+++ 12_genericke_datove_typy/02_02_print_overload.go
@@ -1,11 +1,18 @@
+// Generické datové typy v Go
+//
+// - Go neumožnuje přetěžování funkcí
+// - tento příklad nelze přeložit!
+
 package main
 
 import "fmt"
 
+// přetížená funkce
 func printValue(value string) {
 	fmt.Println(value)
 }
 
+// přetížená funkce
 func printValue(value int) {
 	fmt.Println(value)
 }
