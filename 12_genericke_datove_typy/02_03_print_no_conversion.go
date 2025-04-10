3d0f84dde84d45cb6c84a5b9e57564ea0926140b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-10 09:20:55 +0200

    Updated comments

--- 12_genericke_datove_typy/02_03_print_no_conversion.go
+++ 12_genericke_datove_typy/02_03_print_no_conversion.go
@@ -1,9 +1,17 @@
+// Generické datové typy v Go
+//
+// - Go nepodporuje automatické přetypování
+//   na odvozený datový typ
+// - tento příklad nelze přeložit!
+
 package main
 
 import "fmt"
 
 type Value string
 
+// funkce akceptující hodnotu typu Value
+// ovšem nikoli například řetězec
 func printValue(value Value) {
 	fmt.Println(value)
 }
