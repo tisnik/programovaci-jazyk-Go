1b25d298f2d252630683b91d27adac45d2c1b262
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-11 09:26:27 +0200

    Type parameters, example #1

---
+++ 12_genericke_datove_typy/04_06_add_type_parameters.go
@@ -0,0 +1,22 @@
+// Generické datové typy v Go
+//
+// - generická varianta funkce pro součet dvou
+//   hodnot stejného (generického) typu
+
+package main
+
+import "fmt"
+
+type numeric interface {
+	int | float64 | complex128
+}
+
+func add[T numeric](x T, y T) T {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(1, 2))
+	fmt.Println(add(1.5, 2.6))
+	fmt.Println(add(1i, 2+4i))
+}
