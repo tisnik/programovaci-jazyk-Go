6e8bde378fb6bc5f32db8bda9f767e09d613fecf
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-15 09:15:27 +0200

    Join function implementation - basic variant

---
+++ 12_genericke_datove_typy/06_01_join.go
@@ -0,0 +1,23 @@
+// Generické datové typy v Go
+//
+// - funkce, která spojí textové reprezentace
+//   všech prvků řezu
+// - tento příklad nelze přeložit!
+
+package main
+
+import "fmt"
+
+func join[T any](items []T) (result string) {
+	// průchod všemi prvky řezu
+	for _, value := range items {
+		// spojení textových reprezentací prvků
+		result += value.String()
+		result += ","
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(join([]string{"first", "second"}))
+}
