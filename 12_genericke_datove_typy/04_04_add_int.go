0d53c0a730c6913eb4f5b6c6cabd9ef4b56aae98
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-17 08:51:17 +0200

    Updated comment

--- 12_genericke_datove_typy/04_04_add_int.go
+++ 12_genericke_datove_typy/04_04_add_int.go
@@ -1,12 +1,14 @@
 // Generické datové typy v Go
 //
-// - negenerická (bežná) funkce pro součet
+// - negenerická (běžná) funkce pro součet
 //   dvou hodnot typu celé číslo
 
 package main
 
 import "fmt"
 
+// negenerická (běžná) funkce pro součet
+// dvou hodnot typu celé číslo
 func add(x int, y int) int {
 	return x + y
 }
