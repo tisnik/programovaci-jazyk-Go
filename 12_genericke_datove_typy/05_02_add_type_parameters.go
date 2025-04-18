c14dbd74b2d6cf420d3bfd998c0d982e6cc295ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-18 09:27:41 +0200

    Updated comments

--- 12_genericke_datove_typy/05_02_add_type_parameters.go
+++ 12_genericke_datove_typy/05_02_add_type_parameters.go
@@ -1,3 +1,10 @@
+// Generické datové typy v Go
+//
+// - otestování, že oba parametry funkce
+//   stejného generického typu jsou shodného
+//   aktuálního typu
+// - tento příklad nelze přeložit!
+
 package main
 
 import "fmt"
@@ -11,6 +18,7 @@ func add[T additive](x T, y T) T {
 }
 
 func main() {
+	// volání funkce s předáním hodnot různých typů
 	fmt.Println(add(1, 2))
 	fmt.Println(add(1, "foo"))
 	fmt.Println(add("bar", 1))
