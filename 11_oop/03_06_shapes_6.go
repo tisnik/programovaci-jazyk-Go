4bc73336646bfc42ee4429ea510ad683b160f5e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-22 09:02:23 +0200

    Shapes interface

---
+++ 11_oop/03_06_shapes_6.go
@@ -0,0 +1,81 @@
+// Koncept rozhraní v programovacím jazyku Go
+//
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// deklarace rozhraní s jedinou metodou
+type Shape interface {
+	area() float64
+}
+
+// deklarace uživatelského typu
+type Square struct {
+	sideLength float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (square Square) area() float64 {
+	return square.sideLength * square.sideLength
+}
+
+// deklarace uživatelského typu
+type Rectangle struct {
+	width, height float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (rectangle Rectangle) area() float64 {
+	return rectangle.width * rectangle.height
+}
+
+// deklarace uživatelského typu
+type Circle struct {
+	radius float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (circle Circle) area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+// deklarace uživatelského typu
+type Ellipse struct {
+	a, b float64
+}
+
+// implementace metody předepsané v rozhraní Shape
+func (ellipse Ellipse) area() float64 {
+	return math.Pi * ellipse.a * ellipse.b
+}
+
+// funkce akceptující libovolný typ splňující rozhraní Shape
+func displayArea(shape Shape) {
+	area := shape.area()
+	fmt.Println("Area: ", area)
+}
+
+func printAreas(shapes []Shape) {
+	for _, shape := range shapes {
+		fmt.Printf("Shape: %T %v\n", shape, shape)
+		fmt.Printf("Area: %f\n", shape.area())
+		fmt.Println()
+	}
+}
+
+func main() {
+	shapes := []Shape{
+		Rectangle{width: 100, height: 100},
+		Circle{radius: 100},
+		Ellipse{a: 100, b: 50}}
+
+	printAreas(shapes)
+}
