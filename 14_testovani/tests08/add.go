52ed11e1d49c51f7030872cbfc11f048aad0f14b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-24 08:32:29 +0200

    Updated comments

--- 14_testovani/tests08/add.go
+++ 14_testovani/tests08/add.go
@@ -1,7 +1,12 @@
+// Jednotkové testy v jazyce Go
+//
+// - balíček s funkcí, která se bude testovat
+
 package main
 
 import "fmt"
 
+// testovaná funkce
 func add(x int32, y int32) int32 {
 	return x + y
 }
