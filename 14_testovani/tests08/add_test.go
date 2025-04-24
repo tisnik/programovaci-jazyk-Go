5cd2466ef602422a363b130e52274f5c41e00182
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-24 08:32:03 +0200

    Unit tests #8

---
+++ 14_testovani/tests08/add_test.go
@@ -0,0 +1,14 @@
+// Jednotkové testy v jazyce Go
+//
+// - jednotkový test
+
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := add(1, 2)
+	if result != 3 {
+		t.Error("1+2 should be 3, got ", result, "instead")
+	}
+}
