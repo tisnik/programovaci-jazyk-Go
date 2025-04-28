ff588d26924a4b1dc61d49f5a66fcc6ba92b7690
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-28 09:35:45 +0200

    Updated index file

--- 11_oop/00_index.go
+++ 11_oop/00_index.go
@@ -1,4 +1,88 @@
 // Rozhraní
 //
 // Seznam příkladů uložených v tomto podadresáři:
+//
+// 01_01_method.go:
+// - deklarace záznamu
+// - deklarace metody s příjemcem odpovídajícím záznamu
+// - zavolání metody
+//
+// 01_02_methods_diffent_types.go:
+// - deklarace dvou nezávislých záznamů
+// - deklarace metod s příjemcem odpovídajícím záznamu
+// - zavolání metod
+//
+// 01_03_enlarge_rect_1.go:
+// - metoda, která se snaží modifikovat příjemce
+//
+// 01_03_enlarge_rect_2.go:
+// - metoda, která se snaží modifikovat příjemce
+// - přístup k prvkům příjemce s využitím dereference
+//
+// 01_03_enlarge_rect_3.go:
+// - metoda, která se snaží modifikovat příjemce
+// - zjednodušený přístup k prvkům příjemce bez dereference
+//
+// 02_01_interface.go:
+// - rozhraní s jednou předepsanou metodou
+//
+// 02_02_interface.go:
+// - rozhraní se dvěma předepsanými metodami
+//
+// 02_03_interface.go:
+// - prázdné rozhraní
+//
+// 02_04_skladani.go:
+// - skládání rozhraní
+//
+// 02_05_skladani.go:
+// - skládání rozhraní
+// - hierarchie typu "diamant"
+//
+// 03_01_shapes_1.go:
+// - deklarace rozhraní Shape s jedinou metodou
+// - deklarace uživatelského typu, který nesplňuje rozhraní Shape
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotou, která rozhraní Shape nesplňuje
+//
+// 03_02_shapes_2.go:
+// - deklarace rozhraní Shape s jedinou metodou
+// - deklarace uživatelského typu, který splňuje rozhraní Shape
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotou, která rozhraní Shape splňuje
+//
+// 03_03_shapes_3.go:
+// - deklarace rozhraní Shape s jedinou metodou
+// - deklarace uživatelského typu
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotou, která rozhraní Shape nesplňuje
+//
+// 03_04_shapes_4.go:
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+//
+// 03_05_shapes_5.go:
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+//
+// 03_06_shapes_6.go:
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+//
+// 03_07_more_interfaces.go:
+// - deklarace dvou rozhraní
+// - deklarace uživatelského typu splňujícího obě rozhraní
+//
+// 03_08_more_interfaces.go:
+// - deklarace dvou rozhraní
+// - deklarace uživatelského typu splňujícího obě rozhraní
+// - deklarace funkcí akceptujících libovolný typ
+//   splňující první resp. druhé rozhraní
+//
 
