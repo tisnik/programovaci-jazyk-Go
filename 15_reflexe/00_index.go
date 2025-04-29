73e75069daf4c9483aedb4e49af33f13809c122f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-29 08:52:00 +0200

    Updated index file

--- 15_reflexe/00_index.go
+++ 15_reflexe/00_index.go
@@ -1,27 +1,94 @@
 // Reflexe
 //
 // Seznam příkladů uložených v tomto podadresáři:
-// 00_index.go
-// 02_01_interface_type.go
-// 02_02_typecast.go
-// 03_01_type_assertion.go
-// 03_02_type_assertion_check.go
-// 03_03_type_assertion_check.go
-// 03_04_type_assertion_check.go
-// 03_05_type_assertion_example_1.go
-// 03_06_type_assertion_example_2.go
-// 03_07_type_assertion_example_3.go
-// 04_01_type_switch_1.go
-// 04_02_type_switch_2.go
-// 04_03_type_switch_3.go
-// 04_04_type_switch_4.go
-// 04_05_type_switch_5.go
-// 04_06_type_switch_6.go
-// 05_01_value_of.go
-// 05_02_value_of_type.go
-// 05_03_value_of_interface.go
-// 05_04_value_of_empty_interface.go
-// 05_05_values_of.go
-// 05_06_values_of_type.go
-// 06_01_values_of_nil.go
-// 06_02_values_of_type_nil.go
+//
+// 02_01_interface_type.go:
+// - deklarace rozhraní s jedinou metodou
+// - deklarace uživatelských typů splňujících stejné rozhraní
+// - funkce akceptující libovolný typ splňující rozhraní Shape
+// - zavolání této funkce s hodnotami, které rozhraní Shape splňují
+//
+// 02_02_typecast.go:
+// - deklarace rozhraní s jedinou metodou
+// - pokus o přetypování z rozhraní na konkrétní datový typ
+//
+// 03_01_type_assertion.go:
+// - deklarace rozhraní s jedinou metodou
+// - typová aserce
+//
+// 03_02_type_assertion_check.go:
+// - deklarace rozhraní s jedinou metodou
+// - typová aserce
+// - použití nekorektního rozhraní
+//
+// 03_03_type_assertion_check.go:
+// - deklarace rozhraní s jedinou metodou
+// - typová aserce
+//
+// 03_04_type_assertion_check.go:
+// - deklarace rozhraní s jedinou metodou
+// - typová aserce
+//
+// 03_05_type_assertion_example_1.go:
+// - typová aserce pro parametr typu any
+// - rozlišení mezi hodnotami typu int, bool a string
+//
+// 03_06_type_assertion_example_2.go:
+// - typová aserce pro parametr typu any
+// - rozlišení mezi hodnotami splňujícími rozhraní Inteface1 a Interface2
+//
+// 03_07_type_assertion_example_3.go:
+// - typová aserce pro parametr typu any
+// - rozlišení mezi hodnotami splňujícími rozhraní Inteface1, Interface2,
+//   Type1, Type2 a Type3.
+//
+// 04_01_type_switch_1.go:
+// - konverze hodnoty předané v parametru typu any
+//   na vybrané konkrétní datové typy
+//
+// 04_02_type_switch_2.go:
+// - konverze hodnoty předané v parametru typu any
+//   na vybrané konkrétní datové typy
+// - otestování, že konvertované hodnoty mají skutečně
+//   očekávaný datový typ a lze s nimi provádět operace
+//   specifické pro tento typ
+//
+// 04_03_type_switch_3.go:
+// - konverze hodnoty předané v parametru typu any
+//   na vybraný typ "rozhraní"
+//
+// 04_04_type_switch_4.go:
+// - konverze hodnoty předané v parametru typu any
+//   na vybraný typ "rozhraní"
+//
+// 04_05_type_switch_5.go:
+// - praktická ukázka rozvětvení a konverze na základě
+//   požadovaného datového typu v aplikaci využívající
+//   multimediální knihovnu SDL2
+//
+// 04_06_type_switch_6.go:
+// - praktická ukázka rozvětvení a konverze na základě
+//   požadovaného datového typu v aplikaci využívající
+//   průchod AST stromem (standardní knihovny jazyka Go)
+//
+// 05_01_value_of.go:
+// - funkce reflect.ValueOf
+// - datový typ reflect.Value
+//
+// 05_02_value_of_type.go:
+// - funkce reflect.ValueOf
+// - datový typ reflect.Value
+//
+// 05_03_value_of_interface.go:
+// - funkce reflect.ValueOf a prázdné rozhraní
+// - datový typ reflect.Value
+//
+// 05_04_value_of_empty_interface.go:
+// - funkce reflect.ValueOf a hodnota nil
+// - datový typ reflect.Value
+//
+// 05_05_values_of.go:
+// - funkce reflect.ValueOf a různé předávané hodnoty
+// - datový typ reflect.Value
+//
+// 05_06_values_of_type.go:
