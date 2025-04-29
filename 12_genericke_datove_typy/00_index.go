61fc0d672ff7d3c7fd9dae7ca8aa83c352246c2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-29 08:51:49 +0200

    Updated index file

--- 12_genericke_datove_typy/00_index.go
+++ 12_genericke_datove_typy/00_index.go
@@ -1,4 +1,95 @@
 // Generické datové typy
 //
 // Seznam příkladů uložených v tomto podadresáři:
+//
+// 02_01_print.go:
+// - funkce printValue, která je deklarována
+//   jakožto běžná negenerická funkce
+// - zavolání této funkce
+//
+// 02_02_print_overload.go:
+// - Go neumožňuje přetěžování funkcí
+// - tento příklad nelze přeložit!
+//
+// 02_03_print_no_conversion.go:
+// - Go nepodporuje automatické přetypování
+//   na odvozený datový typ
+// - tento příklad nelze přeložit!
+//
+// 02_04_print_interface.go:
+// - funkce printValue akceptující
+//   hodnotu libovolného typu
+// - využití prázdného rozhraní pro typ parametru
+//
+// 03_01_generic_print.go:
+// - typový parametr u funkce printValue
+// - tuto funkci lze zavolat s předáním libovolného parametru
+//
+// 03_02_type_parameter.go:
+// - vynucení konkrétního typového parametru
+//   při volání funkce printValue
+// - vynucení konkrétního typu parametru funkce
+//   při jejím volání
+//
+// 03_03_type_parameter_check.go:
+// - vynucení konkrétního typového parametru
+//   při volání funkce printValue
+// - tento program nelze přeložit!
+//
+// 04_01_comparable.go:
+// - negenerická varianta funkce pro
+//   porovnání dvou hodnot typu int
+//
+// 04_02_comparable_variable_types.go:
+// - různé varianty funkce pro porovnání
+//   dvou hodnot
+// - každá varianta této funkce má odlišné jméno
+//
+// 04_03_compare_type_parameters.go:
+// - generická varianta funkce pro porovnání
+//   dvou hodnot stejného (generického) typu
+//
+// 04_04_add_int.go:
+// - negenerická (běžná) funkce pro součet
+//   dvou hodnot typu celé číslo
+//
+// 04_05_add_overloaded.go:
+// - pokus o přetížení funkce pro součet
+//   dvou hodnot
+// - tento příklad není možné přeložit!
+//
+// 04_06_add_type_parameters.go:
+// - generická varianta funkce pro součet dvou
+//   hodnot stejného (generického) typu
+//
+// 04_07_add_type_parameters.go:
+// - generická varianta funkce pro součet dvou
+//   hodnot stejného (generického) typu
+// - odvození typů od typů základních
+// - této funkci předáváme hodnoty odvozených typů
+// - tento příklad nelze přeložit!
+//
+// 04_08_add_type_parameters.go:
+// - generická varianta funkce pro součet dvou
+//   hodnot stejného (generického) typu
+// - odvození typů od typů základních
+// - této funkci předáváme hodnoty odvozených typů
+// - aproximace datových typů
+//
+// 05_01_add_type_parameters.go:
+// - deklarace interních proměnných s využitím
+//   generického datového typu
+//
+// 05_02_add_type_parameters.go:
+// - otestování, že oba parametry funkce
+//   stejného generického typu jsou shodného
+//   aktuálního typu
+// - tento příklad nelze přeložit!
+//
+// 05_03_add_type_parameters.go:
+// - funkce s dvojicí generických datových typů
+// - tento příklad nelze přeložit!
+//
+// 05_04_add_type_parameters.go:
+// - funkce s dvojicí generických datových typů
 
