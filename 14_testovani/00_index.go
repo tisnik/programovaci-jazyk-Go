0ceb128e20e4bd5009e3cf30feb6215ae19dbbdb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-29 08:55:56 +0200

    Index file

--- 14_testovani/00_index.go
+++ 14_testovani/00_index.go
@@ -1,4 +1,39 @@
-// Testování
+// Jednotkové testy v jazyce Go
 //
 // Seznam příkladů uložených v tomto podadresáři:
-
+//
+// exchange:
+// - převody měn
+//
+// factorial:
+// - výpočet faktoriálu
+//
+// factorial2:
+// - výpočet faktoriálu
+//
+// mocking:
+// - mocking v Go
+//
+// tests01:
+// - jednotkové testy v Go, základní příklad
+//
+// tests02:
+// - jednotkové testy v Go, základní příklad
+//
+// tests03:
+// - více jednotkových testů
+//
+// tests04:
+// - více jednotkových testů
+//
+// tests05:
+// - testování řízené tabulkou
+//
+// tests06:
+// - testování řízené tabulkou
+//
+// tests07:
+// - testování řízené tabulkami
+//
+// tests08:
+// - tagy pro odlišení testů
